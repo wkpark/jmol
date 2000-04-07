cab76901d87d65ba736cc2d9df73e51da196e46a
ewilligh <ewilligh@users.sourceforge.net>
2000-04-07 11:54:02 +0000

    Read frame energies.

--- Jmol/src/org/openscience/jmol/JMOLANIMATIONConvention.java
+++ Jmol/src/org/openscience/jmol/JMOLANIMATIONConvention.java
@@ -31,8 +31,16 @@ import org.openscience.cml.*;
 
 public class JMOLANIMATIONConvention extends Convention {
 
+  private final int UNKNOWN = -1;
+  private final int ENERGY = 1;
+
+  private int current;
+  private String frame_energy;
+  private String units;
+
   public JMOLANIMATIONConvention(CDOInterface cdo) {
     super(cdo);
+    current = UNKNOWN;
   };
   
   public JMOLANIMATIONConvention(Convention conv) {
@@ -55,15 +63,52 @@ public class JMOLANIMATIONConvention extends Convention {
   public void startElement (String name, AttributeList atts) {
     if (name.equals("list")) {
       System.err.println("Oke, JMOLANIMATION seems to be kicked in :)");
+      super.startElement(name, atts);
+    } else if (name.equals("float")) {
+      boolean isEnergy = false;
+      System.err.println("FLOAT found!");
+      for (int i = 0; i < atts.getLength(); i++) {
+          System.err.println(" att: " + atts.getName(i) + " -> " + atts.getValue(i));
+          if (atts.getName(i).equals("title") && atts.getValue(i).equals("FRAME_ENERGY")) {
+            isEnergy = true;
+          } else if (atts.getName(i).equals("units")) {
+            units = atts.getValue(i); 
+          }
+      }
+      if (isEnergy) {
+	  // oke, this is the frames energy!
+          current = ENERGY;
+      } else {
+        super.startElement(name, atts);
+      }
+    } else {
+      super.startElement(name, atts);
     }
-    super.startElement(name, atts);
   };
 
   public void endElement (String name) {
-    super.endElement(name);
+    if (current == ENERGY) {
+      cdo.setObjectProperty("Frame", "energy", frame_energy); // + " " + units);
+      current = UNKNOWN;
+      frame_energy = "";
+      units = "";
+    } else {
+      super.endElement(name);
+    }
   }
 
   public void characterData (char ch[], int start, int length) {
-    super.characterData(ch, start, length);
+    if (current == ENERGY) {
+      frame_energy = toString(ch, start, length);     
+    } else {
+      super.characterData(ch, start, length);
+    }
+  }
+
+  protected String toString(char ch[], int start, int length) {
+    StringBuffer x = new StringBuffer();
+    for (int i =0; i < length; i++)
+      x.append(ch[start+i]);
+    return x.toString();
   }
 }

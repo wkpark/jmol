3f55ec3fd0fc31b73a72a7ee72adb5e73f1f34d1
ewilligh <ewilligh@users.sourceforge.net>
2000-04-07 09:16:14 +0000

    The JMOLANIMATION convention was removed from the cml.jar since the Java CML Filter Library has since version 1.2.2 means to register custom conventions. JMOLANIMATION is now such a custom convention.

---
+++ Jmol/src/org/openscience/jmol/JMOLANIMATIONConvention.java
@@ -0,0 +1,66 @@
+/*
+ * @(#)JMOLANIMATIONConvention.java   0.1.1 2000/04/07
+ *
+ * Information can be found at http://openscience.chem.nd.edu/~egonw/cml/
+ *
+ * Copyright (c) 2000 E.L. Willighagen (egonw@sci.kun.nl)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+package org.openscience.jmol;
+
+import java.util.*;
+import org.xml.sax.*;
+import com.microstar.xml.*;
+import org.openscience.cdopi.*;
+import org.openscience.cml.*;
+
+public class JMOLANIMATIONConvention extends Convention {
+
+  public JMOLANIMATIONConvention(CDOInterface cdo) {
+    super(cdo);
+  };
+  
+  public JMOLANIMATIONConvention(Convention conv) {
+    super(conv);
+  }
+  
+  public CDOInterface returnCDO() {
+    return this.cdo;
+  };
+  
+  public void startDocument() {
+    super.startDocument();
+  };
+
+  public void endDocument() {
+    super.endDocument();
+  };
+
+  
+  public void startElement (String name, AttributeList atts) {
+    super.startElement(name, atts);
+  };
+
+  public void endElement (String name) {
+    super.endElement(name);
+  }
+
+  public void characterData (char ch[], int start, int length) {
+    super.characterData(ch, start, length);
+  }
+}

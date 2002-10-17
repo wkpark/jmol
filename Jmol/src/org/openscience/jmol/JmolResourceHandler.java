1f20579be8036a439547039b61dd2003dbeedd71
Bradley Smith <yeldar@users.sourceforge.net>
2002-10-17 02:28:53 +0000

    Restored use of Locale since user.language setting is new for Java 1.4.1.

--- Jmol/src/org/openscience/jmol/JmolResourceHandler.java
+++ Jmol/src/org/openscience/jmol/JmolResourceHandler.java
@@ -20,6 +20,7 @@ package org.openscience.jmol;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -40,8 +41,22 @@ class JmolResourceHandler {
   private ResourceBundle generalResourceBundle;
 
   private JmolResourceHandler() {
+    String language = "en";
+    String country = "";
+    String localeString = System.getProperty("user.language");
+    if (localeString != null) {
+      StringTokenizer st = new StringTokenizer(localeString, "_");
+      if (st.hasMoreTokens()) {
+        language = st.nextToken();
+      }
+      if (st.hasMoreTokens()) {
+        country = st.nextToken();
+      }
+    }
+    Locale locale = new Locale(language, country);
     stringsResourceBundle =
-      ResourceBundle.getBundle("org.openscience.jmol.Properties.Jmol");
+      ResourceBundle.getBundle("org.openscience.jmol.Properties.Jmol", locale);
+
     try {
       generalResourceBundle = new PropertyResourceBundle(getClass()
         .getClassLoader()

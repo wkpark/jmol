6a3da1fe4f47a8e0d292af6c9a3bdcb35e180c16
Egon Willighagen <egonw@users.sourceforge.net>
2003-02-10 12:04:59 +0000

    Use java -jar scheme to start Jmol.

--- Jmol/jmol.bat
+++ Jmol/jmol.bat
@@ -4,4 +4,4 @@ rem
 if "%JMOL_HOME%"=="" set JMOL_HOME=.
 
 set libDir=%JMOL_HOME%\jars
-java -Djmol.home="%JMOL_HOME%" -cp "%libDir%\jmol.jar" org.openscience.jmol.Jmol %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djmol.home="%JMOL_HOME%" -jar "%libDir%\jmol.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9

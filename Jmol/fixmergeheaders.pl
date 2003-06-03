2ef7257df5254e9b3470816d89228b93d32cff9e
Michael <michaelthoward@users.sourceforge.net>
2003-06-03 09:34:03 +0000

    don't forget about 'final' !

--- Jmol/fixmergeheaders.pl
+++ Jmol/fixmergeheaders.pl
@@ -32,7 +32,7 @@ sub fixConflict {
 		$state = 'discardingBeforeDoubleBar';
 		next;
 	    }
-	    if ($line =~ /\s*(public|private|abstract|class)/) {
+	    if ($line =~ /\s*(final|public|private|abstract|class)/) {
 		$state = 'copyingFile';
 	    }
 	    print OUT $line;

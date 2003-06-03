3c0e3a35b2ad699a3183f2de125714347590d708
Michael <michaelthoward@users.sourceforge.net>
2003-06-03 11:13:25 +0000

    Explict support files for awt & swing

---
+++ Jmol/src/org/openscience/jmol/g25d/Swing25D.java
@@ -0,0 +1,43 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Jmol Development Team
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+
+package org.openscience.jmol.g25d;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+final public class Swing25D {
+
+  public static Image allocateImage(Component component,
+                                    int width, int height) {
+    return new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+  }
+
+  public static Graphics createGraphics(Image image) {
+    return (image == null) ? null : ((BufferedImage)image).createGraphics();
+  }
+}

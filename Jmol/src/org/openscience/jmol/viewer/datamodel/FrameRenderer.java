cc9add8769995a70939d1d7e415310fcef9cc216
Michael <michaelthoward@users.sourceforge.net>
2003-08-26 18:23:59 +0000

    forgot to add FrameRenderer.java to cvs

---
+++ Jmol/src/org/openscience/jmol/viewer/datamodel/FrameRenderer.java
@@ -0,0 +1,129 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Jmol Development Team
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+
+package org.openscience.jmol.viewer.datamodel;
+
+import org.openscience.jmol.viewer.JmolViewer;
+import org.openscience.jmol.viewer.protein.ProteinProp;
+import org.openscience.jmol.viewer.g3d.Graphics3D;
+import javax.vecmath.Point3d;
+import java.util.Hashtable;
+import java.util.BitSet;
+import java.awt.Rectangle;
+
+public class FrameRenderer {
+
+  JmolViewer viewer;
+  AtomRenderer atomRenderer;
+  BondRenderer bondRenderer;
+  LabelRenderer labelRenderer;
+  DotsRenderer dotsRenderer;
+
+  public FrameRenderer(JmolViewer viewer) {
+    System.out.println("FrameRenderer viewer:" + (viewer != null));
+    this.viewer = viewer;
+    atomRenderer = new AtomRenderer(viewer);
+    bondRenderer = new BondRenderer(viewer);
+    labelRenderer = new LabelRenderer(viewer);
+    dotsRenderer = new DotsRenderer(viewer);
+  }
+  
+  private void setGraphicsContext(Graphics3D g3d, Rectangle rectClip) {
+    atomRenderer.setGraphicsContext(g3d, rectClip);
+    bondRenderer.setGraphicsContext(g3d, rectClip);
+    labelRenderer.setGraphicsContext(g3d, rectClip);
+    dotsRenderer.setGraphicsContext(g3d, rectClip);
+  }
+
+  public void render(Graphics3D g3d, Rectangle rectClip) {
+    JmolFrame frame = viewer.getJmolFrame();
+    if (frame.atomShapeCount <= 0)
+      return;
+
+    setGraphicsContext(g3d, rectClip);
+
+    viewer.calcTransformMatrices();
+
+    dotsRenderer.transform();
+
+    for (int i = frame.atomShapeCount; --i >= 0; ) {
+      AtomShape atomShape = frame.atomShapes[i];
+      atomShape.transform(viewer);
+      dotsRenderer.render(atomShape);
+      atomRenderer.render(atomShape);
+    }
+
+    for (int i = frame.bondShapeCount; --i >= 0; )
+      bondRenderer.render(frame.bondShapes[i]);
+
+    for (int i = frame.lineShapeCount; --i >= 0; ) {
+      LineShape lineShape = frame.lineShapes[i];
+      lineShape.transform(viewer);
+      lineShape.render(g3d, viewer);
+    }
+
+    for (int i = frame.crystalCellLineCount; --i >= 0; ) {
+      LineShape cellLine = frame.crystalCellLines[i];
+      cellLine.transform(viewer);
+      cellLine.render(g3d, viewer);
+    }
+
+    for (int i = frame.measurementShapeCount; --i >= 0; ) {
+      MeasurementShape measurementShape = frame.measurementShapes[i];
+      measurementShape.transform(viewer);
+      measurementShape.render(g3d, viewer);
+    }
+
+    if (viewer.getModeAxes() != JmolViewer.AXES_NONE) {
+      Shape[] axisShapes = viewer.getAxes().getShapes();
+      for (int i = axisShapes.length; --i >= 0; ) {
+        Shape axisShape = axisShapes[i];
+        axisShape.transform(viewer);
+        axisShape.render(g3d, viewer);
+      }
+    }
+
+    if (viewer.getShowBoundingBox()) {
+      Shape[] bboxShapes = viewer.getBoundingBox().getBboxShapes();
+      for (int i = bboxShapes.length; --i >= 0; ) {
+        Shape bboxShape = bboxShapes[i];
+        bboxShape.transform(viewer);
+        bboxShape.render(g3d, viewer);
+      }
+    }
+  }
+
+  public void renderStringOffset(String str, short colix, int points,
+                                 int x, int y, int z,
+                                 int xOffset, int yOffset) {
+    labelRenderer.renderStringOffset(str, colix, points,
+                                     x, y, z, xOffset, yOffset);
+  }
+  
+  public void renderStringOutside(String str, short colix, int pointsFontsize,
+                                  int x, int y, int z) {
+    labelRenderer.renderStringOutside(str, colix, pointsFontsize, x, y, z);
+  }
+}

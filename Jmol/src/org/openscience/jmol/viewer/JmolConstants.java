026e56cbdb03dc0a57587cfbb42623a19e8ae13a
Michael <michaelthoward@users.sourceforge.net>
2003-10-07 11:45:10 +0000

    minor tweaking of color tables

--- Jmol/src/org/openscience/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/openscience/jmol/viewer/JmolConstants.java
@@ -347,7 +347,7 @@ final public class JmolConstants {
    * @see <a href='http://openbabel.sourceforge.net'>openbabel.sourceforge.net</a>
    */
   public final static float[] covalentRadii = {
-    2f,      //   0  Xx big enough to cause trouble
+    2f,      //   0  Xx big enough to bring attention to itself
     0.23f,   //   1  H
     0.93f,   //   2  He
     0.68f,   //   3  Li
@@ -461,8 +461,10 @@ final public class JmolConstants {
 
   /**
    * Default table of CPK atom colors.
-   * Used when the client does not implement getAtomColor(clientAtom, colorScheme)
-   * I didn't know what color to define for many of the atoms, so I made them pink.
+   * Used when the client does not implement
+   * getAtomColor(clientAtom, colorScheme)
+   * I didn't know what color to define for many of the atoms,
+   * so I made them HotPink.
    */
   public final static int[] argbsCpk = {
     0xFFFF69B4, // HotPink        Xx   0
@@ -850,153 +852,153 @@ final public class JmolConstants {
   };
 
   public final static int[] colorArgbs = {
-    0xFFF0F8FF,
-    0xFFFAEBD7,
-    0xFF00FFFF,
-    0xFF7FFFD4,
-    0xFFF0FFFF,
-    0xFFF5F5DC,
-    0xFFFFE4C4,
-    0xFF000000,
-    0xFFFFEBCD,
-    0xFF0000FF,
-    0xFF8A2BE2,
-    0xFFA52A2A,
-    0xFFDEB887,
-    0xFF5F9EA0,
-    0xFF7FFF00,
-    0xFFD2691E,
-    0xFFFF7F50,
-    0xFF6495ED,
-    0xFFFFF8DC,
-    0xFFDC143C,
-    0xFF00FFFF,
-    0xFF00008B,
-    0xFF008B8B,
-    0xFFB8860B,
-    0xFFA9A9A9,
-    0xFF006400,
-    0xFFBDB76B,
-    0xFF8B008B,
-    0xFF556B2F,
-    0xFFFF8C00,
-    0xFF9932CC,
-    0xFF8B0000,
-    0xFFE9967A,
-    0xFF8FBC8F,
-    0xFF483D8B,
-    0xFF2F4F4F,
-    0xFF00CED1,
-    0xFF9400D3,
-    0xFFFF1493,
-    0xFF00BFFF,
-    0xFF696969,
-    0xFF1E90FF,
-    0xFFB22222,
-    0xFFFFFAF0,
-    0xFF228B22,
-    0xFFFF00FF,
-    0xFFDCDCDC,
-    0xFFF8F8FF,
-    0xFFFFD700,
-    0xFFDAA520,
-    0xFF808080,
-    0xFF008000,
-    0xFFADFF2F,
-    0xFFF0FFF0,
-    0xFFFF69B4,
-    0xFFCD5C5C,
-    0xFF4B0082,
-    0xFFFFFFF0,
-    0xFFF0E68C,
-    0xFFE6E6FA,
-    0xFFFFF0F5,
-    0xFF7CFC00,
-    0xFFFFFACD,
-    0xFFADD8E6,
-    0xFFF08080,
-    0xFFE0FFFF,
-    0xFFFAFAD2,
-    0xFF90EE90,
-    0xFFD3D3D3,
-    0xFFFFB6C1,
-    0xFFFFA07A,
-    0xFF20B2AA,
-    0xFF87CEFA,
-    0xFF778899,
-    0xFFB0C4DE,
-    0xFFFFFFE0,
-    0xFF00FF00,
-    0xFF32CD32,
-    0xFFFAF0E6,
-    0xFFFF00FF,
-    0xFF800000,
-    0xFF66CDAA,
-    0xFF0000CD,
-    0xFFBA55D3,
-    0xFF9370DB,
-    0xFF3CB371,
-    0xFF7B68EE,
-    0xFF00FA9A,
-    0xFF48D1CC,
-    0xFFC71585,
-    0xFF191970,
-    0xFFF5FFFA,
-    0xFFFFE4E1,
-    0xFFFFE4B5,
-    0xFFFFDEAD,
-    0xFF000080,
-    0xFFFDF5E6,
-    0xFF808000,
-    0xFF6B8E23,
-    0xFFFFA500,
-    0xFFFF4500,
-    0xFFDA70D6,
-    0xFFEEE8AA,
-    0xFF98FB98,
-    0xFFAFEEEE,
-    0xFFDB7093,
-    0xFFFFEFD5,
-    0xFFFFDAB9,
-    0xFFCD853F,
-    0xFFFFC0CB,
-    0xFFDDA0DD,
-    0xFFB0E0E6,
-    0xFF800080,
-    0xFFFF0000,
-    0xFFBC8F8F,
-    0xFF4169E1,
-    0xFF8B4513,
-    0xFFFA8072,
-    0xFFF4A460,
-    0xFF2E8B57,
-    0xFFFFF5EE,
-    0xFFA0522D,
-    0xFFC0C0C0,
-    0xFF87CEEB,
-    0xFF6A5ACD,
-    0xFF708090,
-    0xFFFFFAFA,
-    0xFF00FF7F,
-    0xFF4682B4,
-    0xFFD2B48C,
-    0xFF008080,
-    0xFFD8BFD8,
-    0xFFFF6347,
-    0xFF40E0D0,
-    0xFFEE82EE,
-    0xFFF5DEB3,
-    0xFFFFFFFF,
-    0xFFF5F5F5,
-    0xFFFFFF00,
-    0xFF9ACD32,
-    // RasMol values
-    0xFFAFD7FF,
-    0xFF2E8B57,
-    0xFF98FFB3,
-    0xFF808080,
-    0xFFFFABBB,
-    0xFFFF4500,
-    0xFFF6F675,
+    0xFFF0F8FF, // aliceblue
+    0xFFFAEBD7, // antiquewhite
+    0xFF00FFFF, // aqua
+    0xFF7FFFD4, // aquamarine
+    0xFFF0FFFF, // azure
+    0xFFF5F5DC, // beige
+    0xFFFFE4C4, // bisque
+    0xFF000000, // black
+    0xFFFFEBCD, // blanchedalmond
+    0xFF0000FF, // blue
+    0xFF8A2BE2, // blueviolet
+    0xFFA52A2A, // brown
+    0xFFDEB887, // burlywood
+    0xFF5F9EA0, // cadetblue
+    0xFF7FFF00, // chartreuse
+    0xFFD2691E, // chocolate
+    0xFFFF7F50, // coral
+    0xFF6495ED, // cornflowerblue
+    0xFFFFF8DC, // cornsilk
+    0xFFDC143C, // crimson
+    0xFF00FFFF, // cyan
+    0xFF00008B, // darkblue
+    0xFF008B8B, // darkcyan
+    0xFFB8860B, // darkgoldenrod
+    0xFFA9A9A9, // darkgray
+    0xFF006400, // darkgreen
+    0xFFBDB76B, // darkkhaki
+    0xFF8B008B, // darkmagenta
+    0xFF556B2F, // darkolivegreen
+    0xFFFF8C00, // darkorange
+    0xFF9932CC, // darkorchid
+    0xFF8B0000, // darkred
+    0xFFE9967A, // darksalmon
+    0xFF8FBC8F, // darkseagreen
+    0xFF483D8B, // darkslateblue
+    0xFF2F4F4F, // darkslategray
+    0xFF00CED1, // darkturquoise
+    0xFF9400D3, // darkviolet
+    0xFFFF1493, // deeppink
+    0xFF00BFFF, // deepskyblue
+    0xFF696969, // dimgray
+    0xFF1E90FF, // dodgerblue
+    0xFFB22222, // firebrick
+    0xFFFFFAF0, // floralwhite
+    0xFF228B22, // forestgreen
+    0xFFFF00FF, // fuchsia
+    0xFFDCDCDC, // gainsboro
+    0xFFF8F8FF, // ghostwhite
+    0xFFFFD700, // gold
+    0xFFDAA520, // goldenrod
+    0xFF808080, // gray
+    0xFF008000, // green
+    0xFFADFF2F, // greenyellow
+    0xFFF0FFF0, // honeydew
+    0xFFFF69B4, // hotpink
+    0xFFCD5C5C, // indianred
+    0xFF4B0082, // indigo
+    0xFFFFFFF0, // ivory
+    0xFFF0E68C, // khaki
+    0xFFE6E6FA, // lavender
+    0xFFFFF0F5, // lavenderblush
+    0xFF7CFC00, // lawngreen
+    0xFFFFFACD, // lemonchiffon
+    0xFFADD8E6, // lightblue
+    0xFFF08080, // lightcoral
+    0xFFE0FFFF, // lightcyan
+    0xFFFAFAD2, // lightgoldenrodyellow
+    0xFF90EE90, // lightgreen
+    0xFFD3D3D3, // lightgrey
+    0xFFFFB6C1, // lightpink
+    0xFFFFA07A, // lightsalmon
+    0xFF20B2AA, // lightseagreen
+    0xFF87CEFA, // lightskyblue
+    0xFF778899, // lightslategray
+    0xFFB0C4DE, // lightsteelblue
+    0xFFFFFFE0, // lightyellow
+    0xFF00FF00, // lime
+    0xFF32CD32, // limegreen
+    0xFFFAF0E6, // linen
+    0xFFFF00FF, // magenta
+    0xFF800000, // maroon
+    0xFF66CDAA, // mediumaquamarine
+    0xFF0000CD, // mediumblue
+    0xFFBA55D3, // mediumorchid
+    0xFF9370DB, // mediumpurple
+    0xFF3CB371, // mediumseagreen
+    0xFF7B68EE, // mediumslateblue
+    0xFF00FA9A, // mediumspringgreen
+    0xFF48D1CC, // mediumturquoise
+    0xFFC71585, // mediumvioletred
+    0xFF191970, // midnightblue
+    0xFFF5FFFA, // mintcream
+    0xFFFFE4E1, // mistyrose
+    0xFFFFE4B5, // moccasin
+    0xFFFFDEAD, // navajowhite
+    0xFF000080, // navy
+    0xFFFDF5E6, // oldlace
+    0xFF808000, // olive
+    0xFF6B8E23, // olivedrab
+    0xFFFFA500, // orange
+    0xFFFF4500, // orangered
+    0xFFDA70D6, // orchid
+    0xFFEEE8AA, // palegoldenrod
+    0xFF98FB98, // palegreen
+    0xFFAFEEEE, // paleturquoise
+    0xFFDB7093, // palevioletred
+    0xFFFFEFD5, // papayawhip
+    0xFFFFDAB9, // peachpuff
+    0xFFCD853F, // peru
+    0xFFFFC0CB, // pink
+    0xFFDDA0DD, // plum
+    0xFFB0E0E6, // powderblue
+    0xFF800080, // purple
+    0xFFFF0000, // red
+    0xFFBC8F8F, // rosybrown
+    0xFF4169E1, // royalblue
+    0xFF8B4513, // saddlebrown
+    0xFFFA8072, // salmon
+    0xFFF4A460, // sandybrown
+    0xFF2E8B57, // seagreen
+    0xFFFFF5EE, // seashell
+    0xFFA0522D, // sienna
+    0xFFC0C0C0, // silver
+    0xFF87CEEB, // skyblue
+    0xFF6A5ACD, // slateblue
+    0xFF708090, // slategray
+    0xFFFFFAFA, // snow
+    0xFF00FF7F, // springgreen
+    0xFF4682B4, // steelblue
+    0xFFD2B48C, // tan
+    0xFF008080, // teal
+    0xFFD8BFD8, // thistle
+    0xFFFF6347, // tomato
+    0xFF40E0D0, // turquoise
+    0xFFEE82EE, // violet
+    0xFFF5DEB3, // wheat
+    0xFFFFFFFF, // white
+    0xFFF5F5F5, // whitesmoke
+    0xFFFFFF00, // yellow
+    0xFF9ACD32, // yellowgreen
+    // plus a few rasmol names/values
+    0xFFAFD7FF, // bluetint
+    0xFF2E8B57, // greenblue
+    0xFF98FFB3, // greentint
+    0xFF808080, // grey
+    0xFFFFABBB, // pinktint
+    0xFFFF4500, // redorange
+    0xFFF6F675, // yellowtint
   };
 }

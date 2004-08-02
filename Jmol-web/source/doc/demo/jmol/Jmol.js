8e760ecc62fe3d97a3dbe9dff7e7730de916cae2
Miguel <migueljmol@users.sourceforge.net>
2004-08-02 17:39:49 +0000

    rework and doc for Jmol.js

--- Jmol-web/source/doc/demo/jmol/Jmol.js
+++ Jmol-web/source/doc/demo/jmol/Jmol.js
@@ -23,11 +23,17 @@
  *  02111-1307  USA.
  */
 
-function jmolDebugAlert(enabled) {
-  _jmol.debugAlert = (enabled == undefined || enabled)
+function jmolDebugAlert(enableAlerts) {
+  _jmol.debugAlert = (enableAlerts == undefined || enableAlerts)
 }
 
-function jmolSetColor(bgcolor, boxfgcolor, progresscolor, boxbgcolor) {
+function jmolSetCodebase(codebase) {
+  _jmol.codebase = codebase ? codebase : ".";
+  if (_jmol.debugAlert)
+    alert("jmolCodebase=" + _jmol.codebase);
+}
+
+function jmolSetAppletColor(bgcolor, boxfgcolor, progresscolor, boxbgcolor) {
   _jmol.bgcolor = bgcolor;
   _jmol.boxbgcolor = boxbgcolor ? boxbgcolor : bgcolor;
   if (boxfgcolor)
@@ -44,74 +50,8 @@ function jmolSetColor(bgcolor, boxfgcolor, progresscolor, boxbgcolor) {
 
 }
 
-function jmolSetCodebase(codebase) {
-  _jmol.codebase = codebase ? codebase : ".";
-  if (_jmol.debugAlert)
-    alert("jmolCodebase=" + _jmol.codebase);
-}
-
-function jmolSetButtonCssClass(buttonCssClass) {
-  _jmol.buttonCssClass = buttonCssClass;
-  _jmol.buttonCssText =
-    buttonCssClass ? "class='" + buttonCssClass + "' " : "";
-}
-
-function jmolSetCheckboxCssClass(checkboxCssClass) {
-  _jmol.checkboxCssClass = checkboxCssClass;
-  _jmol.checkboxCssText =
-    checkboxCssClass ? "class='" + checkboxCssClass + "' " : "";
-}
-
-function jmolSetRadioCssClass(radioCssClass) {
-  _jmol.radioCssClass = radioCssClass;
-  _jmol.radioCssText = radioCssClass ? "class='" + radioCssClass + "' " : "";
-}
-
-function jmolSetLinkCssClass(linkCssClass) {
-  _jmol.linkCssClass = linkCssClass;
-  _jmol.linkCssText = linkCssClass ? "class='" + linkCssClass + "' " : "";
-}
-
 function jmolApplet(size, modelFilename, script, nameSuffix) {
   _jmolApplet(size, modelFilename, null, script, nameSuffix);
-}
-
-function jmolAppletInline(size, inlineModel, script, nameSuffix) {
-  _jmolApplet(size, null, _jmolConvertInline(inlineModel), script, nameSuffix);
-}
-
-function _jmolConvertInline(model) {
-  var inlineModel = model.replace(/\r|\n|\r\n/g, "|");
-  if (_jmol.debugAlert)
-    alert("inline model:\n" + inlineModel);
-  return inlineModel;
-}
-
-function jmolScript(script, targetSuffix) {
-  if (script) {
-    var target = "jmol" + (targetSuffix ? targetSuffix : "0");
-    if (document.getElementById)
-      document.getElementById(target).script(script);
-    else
-      document[target].script(script); // NS4 compatibility
-  }
-}
-
-function jmolLoadInline(model, targetSuffix) {
-  if (model) {
-    var target = "jmol" + (targetSuffix ? targetSuffix : "0");
-    var applet;
-    if (document.getElementById)
-      applet = document.getElementById(target);
-    else
-      applet = document[target]; // NS4 compatibility
-    applet.loadInline(model);
-  }
-}
-
-function jmolSetTarget(targetSuffix) {
-  _jmol.targetSuffix = targetSuffix;
-  _jmol.targetText = targetSuffix ? ",\"" + targetSuffix + "\"" : "";
 }
 
 function jmolButton(script, label) {
@@ -153,31 +93,11 @@ function jmolCheckbox(scriptWhenChecked, scriptWhenUnchecked,
   document.close();
 }
 
-function jmolStartRadioGroup() {
-  ++_jmol.radioGroupCount;
-}
-
-function jmolRadio(script, labelHtml, isChecked) {
-  if (!script)
-    return;
-  if (labelHtml == undefined || labelHtml == null)
-    labelHtml = script.substring(0, 32) + "<br />";
-  var scriptIndex = _jmolAddScript(script);
-  var t = "<input name='" + "jmolGroup" + _jmol.radioGroupCount +
-          "' type='radio' onClick='_jmolClick(" + scriptIndex +
-          _jmol.targetText +
-          ")' onMouseover='_jmolMouseOver(" + scriptIndex +
-          ");return true' onMouseout='_jmolMouseOut()' " +
-	  (isChecked ? "checked " : "") + _jmol.radioCssText + "/>" +
-          labelHtml;
-  document.open();
-  document.write(t);
-  document.close();
-}
-
-function jmolRadioGroup(arrayOfRadioButtons) {
+function jmolRadioGroup(arrayOfRadioButtons, separatorHtml) {
   var type = typeof arrayOfRadioButtons;
   if (type == "object" && type != null) {
+    if (separatorHtml == undefined || separatorHtml == null)
+      separatorHtml = "&nbsp; ";
     jmolStartRadioGroup();
     var length = arrayOfRadioButtons.length;
     var i;
@@ -185,9 +105,9 @@ function jmolRadioGroup(arrayOfRadioButtons) {
       var radio = arrayOfRadioButtons[i];
       type = typeof radio;
       if (type == "object") {
-        jmolRadio(radio[0], radio[1], radio[2]);
+        jmolRadio(radio[0], radio[1], radio[2], separatorHtml);
       } else {
-        jmolRadio(radio);
+        jmolRadio(radio, null, null, separatorHtml);
       }
     }
   }
@@ -218,6 +138,96 @@ function jmolBr() {
 }
 
 ////////////////////////////////////////////////////////////////
+// advanced scripting functions
+////////////////////////////////////////////////////////////////
+
+function jmolAppletInline(size, inlineModel, script, nameSuffix) {
+  _jmolApplet(size, null, _jmolConvertInline(inlineModel), script, nameSuffix);
+}
+
+function jmolSetTarget(targetSuffix) {
+  _jmol.targetSuffix = targetSuffix;
+  _jmol.targetText = targetSuffix ? ",\"" + targetSuffix + "\"" : "";
+}
+
+function jmolScript(script, targetSuffix) {
+  if (script) {
+    var target = "jmol" + (targetSuffix ? targetSuffix : "0");
+    if (document.getElementById)
+      document.getElementById(target).script(script);
+    else
+      document[target].script(script); // NS4 compatibility
+  }
+}
+
+function jmolLoadInline(model, targetSuffix) {
+  if (model) {
+    var target = "jmol" + (targetSuffix ? targetSuffix : "0");
+    var applet;
+    if (document.getElementById)
+      applet = document.getElementById(target);
+    else
+      applet = document[target]; // NS4 compatibility
+    applet.loadInline(model);
+  }
+}
+
+function jmolStartRadioGroup() {
+  ++_jmol.radioGroupCount;
+}
+
+function jmolRadio(script, labelHtml, isChecked, separatorHtml) {
+  if (!script)
+    return;
+  if (labelHtml == undefined || labelHtml == null)
+    labelHtml = script.substring(0, 32);
+  if (! separatorHtml)
+    separatorHtml = "";
+  var scriptIndex = _jmolAddScript(script);
+  var t = "<input name='" + "jmolGroup" + _jmol.radioGroupCount +
+          "' type='radio' onClick='_jmolClick(" + scriptIndex +
+          _jmol.targetText +
+          ")' onMouseover='_jmolMouseOver(" + scriptIndex +
+          ");return true' onMouseout='_jmolMouseOut()' " +
+	  (isChecked ? "checked " : "") + _jmol.radioCssText + "/>" +
+          labelHtml + separatorHtml;
+  document.open();
+  document.write(t);
+  document.close();
+}
+
+////////////////////////////////////////////////////////////////
+// Cascading Style Sheet Class support
+////////////////////////////////////////////////////////////////
+function jmolSetAppletCssClass(appletCssClass) {
+  _jmol.appletCssClass = appletCssClass;
+  _jmol.appletCssText =
+    appletCssClass ? "class='" + appletCssClass + "' " : "";
+}
+
+function jmolSetButtonCssClass(buttonCssClass) {
+  _jmol.buttonCssClass = buttonCssClass;
+  _jmol.buttonCssText =
+    buttonCssClass ? "class='" + buttonCssClass + "' " : "";
+}
+
+function jmolSetCheckboxCssClass(checkboxCssClass) {
+  _jmol.checkboxCssClass = checkboxCssClass;
+  _jmol.checkboxCssText =
+    checkboxCssClass ? "class='" + checkboxCssClass + "' " : "";
+}
+
+function jmolSetRadioCssClass(radioCssClass) {
+  _jmol.radioCssClass = radioCssClass;
+  _jmol.radioCssText = radioCssClass ? "class='" + radioCssClass + "' " : "";
+}
+
+function jmolSetLinkCssClass(linkCssClass) {
+  _jmol.linkCssClass = linkCssClass;
+  _jmol.linkCssText = linkCssClass ? "class='" + linkCssClass + "' " : "";
+}
+
+////////////////////////////////////////////////////////////////
 // functions for INTERNAL USE ONLY which are subject to change
 // use at your own risk ... you have been WARNED!
 ////////////////////////////////////////////////////////////////
@@ -237,6 +247,8 @@ appletCount: 0,
 
 radioGroupCount: 0,
 
+appletCssClass: null,
+appletCssText: "",
 buttonCssClass: null,
 buttonCssText: "",
 checkboxCssClass: null,
@@ -260,10 +272,11 @@ function _jmolApplet(size, modelFilename, inlineModel, script, nameSuffix) {
       script = "select *";
     var sz = _jmolGetAppletSize(size);
     var t;
-    t = "<applet name='jmol" + nameSuffix + "' id='jmol" + nameSuffix + 
-        "' code='JmolApplet' archive='JmolApplet.jar'\n" +
-        "  codebase='" + codebase + "'\n" +
-        "  width='" + sz[0] + "' height='" + sz[1] +
+    t = "<applet name='jmol" + nameSuffix + "' id='jmol" + nameSuffix +
+        "' " + appletCssClass +
+        " code='JmolApplet' archive='JmolApplet.jar'\n" +
+        " codebase='" + codebase + "'\n" +
+        " width='" + sz[0] + "' height='" + sz[1] +
         "' mayscript='true'>\n" +
         "  <param name='progressbar' value='true' />\n" +
         "  <param name='progresscolor' value='" +
@@ -292,6 +305,13 @@ function _jmolApplet(size, modelFilename, inlineModel, script, nameSuffix) {
     document.write(t);
     document.close(); // NS4 compatibility
   }
+}
+
+function _jmolConvertInline(model) {
+  var inlineModel = model.replace(/\r|\n|\r\n/g, "|");
+  if (_jmol.debugAlert)
+    alert("inline model:\n" + inlineModel);
+  return inlineModel;
 }
 
 function _jmolGetAppletSize(size) {

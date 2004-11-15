a1247520ecd62820c3b5b5457f46003ad7c16de5
Rene Kanters <rkanters@users.sourceforge.net>
2004-11-15 22:06:23 +0000

    Gaussian reader now puts the geometries for 'scan' run in their own 'directories'.

--- Jmol/src/org/jmol/adapter/smarter/GaussianReader.java
+++ Jmol/src/org/jmol/adapter/smarter/GaussianReader.java
@@ -33,7 +33,7 @@ import java.io.IOException;
  *
  **/
 class GaussianReader extends AtomSetCollectionReader {
- 
+  
   /**
    * Word index of atomic number in line with atom coordinates in an
    * orientation block.
@@ -44,7 +44,7 @@ class GaussianReader extends AtomSetCollectionReader {
    * frequency output.
    */
   private final static int FREQ_FIRST_VECTOR_OFFSET = 2;
-
+  
   /**
    * The default offset for the coordinate output is that for G98 or G03.
    * If it turns out to be a G94 file, this will be reset.
@@ -57,9 +57,12 @@ class GaussianReader extends AtomSetCollectionReader {
    * Key of the type of SCF energy calculated, e.g., E(RB+HF-PW91).
    */
   private String scfKey = "";
-    
+  
   /** The number of the calculation being interpreted */
   private int calculationNumber = 1;
+  
+  /** The scan point. -1 denotes no scan information */
+  private int scanPoint = -1;
   
   /**
    * Reads a Collection of AtomSets from a BufferedReader.
@@ -88,10 +91,10 @@ class GaussianReader extends AtomSetCollectionReader {
    * @throws Exception If an error occurs
    **/
   AtomSetCollection readAtomSetCollection(BufferedReader reader)
-    throws Exception {
-
+  throws Exception {
+    
     atomSetCollection = new AtomSetCollection("gaussian");
- 
+    
     try {
       String line;
       int lineNum = 0;
@@ -107,7 +110,20 @@ class GaussianReader extends AtomSetCollectionReader {
       int nOrientations = 0;
       while ((line = reader.readLine()) != null) {
         if (line.startsWith(" Step number")) {
-          nOrientations = 0;
+          nOrientations = 0; // reset the number of orientations
+          // check for scan point information
+          int scanPointIndex= line.indexOf("scan point");
+          if (scanPointIndex>0) {
+            scanPoint = parseInt(line,scanPointIndex+10);
+            System.out.println("Scan point is " + scanPoint);
+          } else {
+            scanPoint = -1; // no scan point information
+          }
+        } else if (line.indexOf("-- Stationary point found")>0) {
+          // stationary point, if have scanPoint: need to increment now...
+          // to get the initial geometry for the next scan point in the proper
+          // place
+          if (scanPoint>=0) scanPoint++;
         } else if (line.indexOf("Input orientation:") >= 0 ||
             line.indexOf("Z-Matrix orientation:") >= 0) {
           ++nOrientations; // is always the first orientation
@@ -120,7 +136,7 @@ class GaussianReader extends AtomSetCollectionReader {
         } else if (line.startsWith(" Harmonic frequencies")) {
           readFrequencies(reader);
         } else if (line.startsWith(" Total atomic charges:") ||
-                   line.startsWith(" Mulliken atomic charges:")) {
+            line.startsWith(" Mulliken atomic charges:")) {
           // NB this only works for the Standard or Input orientation of
           // the molecule since it does not list the values for the
           // dummy atoms in the z-matrix
@@ -145,7 +161,7 @@ class GaussianReader extends AtomSetCollectionReader {
     
     return atomSetCollection;
   }
- 
+  
   /**
    * Interprets the SCF Done: section.
    *
@@ -161,18 +177,18 @@ class GaussianReader extends AtomSetCollectionReader {
    * @throws Exception If an error occurs
    **/
   private void readSCFDone(BufferedReader reader,
-                           String line,
-                           int nOrientations) throws Exception {
+      String line,
+      int nOrientations) throws Exception {
     String tokens[] = getTokens(line,11);
     scfKey = tokens[0];
     scfEnergy = tokens[2]+" "+tokens[3];
     // now set the names for the last nOrientations
     for (int asci = atomSetCollection.currentAtomSetIndex, n = nOrientations;
-         --n >= 0;
-         --asci) {
+    --n >= 0;
+    --asci) {
       atomSetCollection.setAtomSetName(
-        scfKey+" = " + scfEnergy + " " +
-        atomSetCollection.getAtomSetName(asci), asci
+          scfKey+" = " + scfEnergy + " " +
+          atomSetCollection.getAtomSetName(asci), asci
       );
     }
     // also set the properties for them
@@ -196,41 +212,41 @@ class GaussianReader extends AtomSetCollectionReader {
     }    
   }
   
-/* GAUSSIAN STRUCTURAL INFORMATION THAT IS EXPECTED
-     NB I currently use the firstCoordinateOffset value to determine where
-     X starts, I could use the number of tokens - 3, and read the last 3...
-*/
-
-// GAUSSIAN 04 format
-/*                 Standard orientation:
- ----------------------------------------------------------
- Center     Atomic              Coordinates (Angstroms)
- Number     Number             X           Y           Z
- ----------------------------------------------------------
- 1          6           0.000000    0.000000    1.043880
- ##SNIP##    
- ---------------------------------------------------------------------
-*/
-
-// GAUSSIAN 98 and 03 format
-/*                    Standard orientation:                         
- ---------------------------------------------------------------------
- Center     Atomic     Atomic              Coordinates (Angstroms)
- Number     Number      Type              X           Y           Z
- ---------------------------------------------------------------------
- 1          6             0        0.852764   -0.020119    0.050711
- ##SNIP##
- ---------------------------------------------------------------------
-*/
-
+  /* GAUSSIAN STRUCTURAL INFORMATION THAT IS EXPECTED
+   NB I currently use the firstCoordinateOffset value to determine where
+   X starts, I could use the number of tokens - 3, and read the last 3...
+   */
+  
+  // GAUSSIAN 04 format
+  /*                 Standard orientation:
+   ----------------------------------------------------------
+   Center     Atomic              Coordinates (Angstroms)
+   Number     Number             X           Y           Z
+   ----------------------------------------------------------
+   1          6           0.000000    0.000000    1.043880
+   ##SNIP##    
+   ---------------------------------------------------------------------
+   */
+  
+  // GAUSSIAN 98 and 03 format
+  /*                    Standard orientation:                         
+   ---------------------------------------------------------------------
+   Center     Atomic     Atomic              Coordinates (Angstroms)
+   Number     Number      Type              X           Y           Z
+   ---------------------------------------------------------------------
+   1          6             0        0.852764   -0.020119    0.050711
+   ##SNIP##
+   ---------------------------------------------------------------------
+   */
+  
   private void readAtoms(BufferedReader reader, String line) throws Exception {
     atomSetCollection.newAtomSet();
-    atomSetCollection.setAtomSetName(line.trim());
-    String path = getTokens(line)[0];
+    atomSetCollection.setAtomSetName(""); // start with an empty name
+    String path = getTokens(line)[0]; // path = type of orientation
     discardLines(reader, 4);
     String tokens[];
     while ((line = reader.readLine()) != null &&
-           !line.startsWith(" --")) {
+        !line.startsWith(" --")) {
       tokens = getTokens(line); // get the tokens in the line
       Atom atom = atomSetCollection.addNewAtom();
       atom.elementNumber =
@@ -242,50 +258,51 @@ class GaussianReader extends AtomSetCollectionReader {
       atom.y = parseFloat(tokens[++offset]);
       atom.z = parseFloat(tokens[++offset]);
     }
-    // the path will be the calculation number and the type of orientation
     atomSetCollection.setAtomSetProperty(SmarterJmolAdapter.PATH_KEY,
-      calculationNumber+SmarterJmolAdapter.PATH_SEPARATOR+path);
+        "Calculation "+calculationNumber+
+        (scanPoint>=0?(SmarterJmolAdapter.PATH_SEPARATOR+"Scan Point "+scanPoint):"")+
+        SmarterJmolAdapter.PATH_SEPARATOR+path);
   }
-
-/* SAMPLE FREQUENCY OUTPUT */
-/*
- Harmonic frequencies (cm**-1), IR intensities (KM/Mole), Raman scattering
- activities (A**4/AMU), depolarization ratios for plane and unpolarized
- incident light, reduced masses (AMU), force constants (mDyne/A),
- and normal coordinates:
-                     1                      2                      3
-                    A1                     B2                     B1
- Frequencies --    64.6809                64.9485               203.8241
- Red. masses --     8.0904                 2.2567                 1.0164
- Frc consts  --     0.0199                 0.0056                 0.0249
- IR Inten    --     1.4343                 1.4384                15.8823
- Atom AN      X      Y      Z        X      Y      Z        X      Y      Z
- 1   6     0.00   0.00   0.48     0.00  -0.05   0.23     0.01   0.00   0.00
- 2   6     0.00   0.00   0.48     0.00  -0.05  -0.23     0.01   0.00   0.00
- 3   1     0.00   0.00   0.49     0.00  -0.05   0.63     0.03   0.00   0.00
- 4   1     0.00   0.00   0.49     0.00  -0.05  -0.63     0.03   0.00   0.00
- 5   1     0.00   0.00  -0.16     0.00  -0.31   0.00    -1.00   0.00   0.00
- 6  35     0.00   0.00  -0.16     0.00   0.02   0.00     0.01   0.00   0.00
- ##SNIP##
- 10                     11                     12
- A1                     B2                     A1
- Frequencies --  2521.0940              3410.1755              3512.0957
- Red. masses --     1.0211                 1.0848                 1.2333
- Frc consts  --     3.8238                 7.4328                 8.9632
- IR Inten    --   264.5877               109.0525                 0.0637
- Atom AN      X      Y      Z        X      Y      Z        X      Y      Z
- 1   6     0.00   0.00   0.00     0.00   0.06   0.00     0.00  -0.10   0.00
- 2   6     0.00   0.00   0.00     0.00   0.06   0.00     0.00   0.10   0.00
- 3   1     0.00   0.01   0.00     0.00  -0.70   0.01     0.00   0.70  -0.01
- 4   1     0.00  -0.01   0.00     0.00  -0.70  -0.01     0.00  -0.70  -0.01
- 5   1     0.00   0.00   1.00     0.00   0.00   0.00     0.00   0.00   0.00
- 6  35     0.00   0.00  -0.01     0.00   0.00   0.00     0.00   0.00   0.00
- 
- -------------------
- - Thermochemistry -
- -------------------
-*/
-
+  
+  /* SAMPLE FREQUENCY OUTPUT */
+  /*
+   Harmonic frequencies (cm**-1), IR intensities (KM/Mole), Raman scattering
+   activities (A**4/AMU), depolarization ratios for plane and unpolarized
+   incident light, reduced masses (AMU), force constants (mDyne/A),
+   and normal coordinates:
+   1                      2                      3
+   A1                     B2                     B1
+   Frequencies --    64.6809                64.9485               203.8241
+   Red. masses --     8.0904                 2.2567                 1.0164
+   Frc consts  --     0.0199                 0.0056                 0.0249
+   IR Inten    --     1.4343                 1.4384                15.8823
+   Atom AN      X      Y      Z        X      Y      Z        X      Y      Z
+   1   6     0.00   0.00   0.48     0.00  -0.05   0.23     0.01   0.00   0.00
+   2   6     0.00   0.00   0.48     0.00  -0.05  -0.23     0.01   0.00   0.00
+   3   1     0.00   0.00   0.49     0.00  -0.05   0.63     0.03   0.00   0.00
+   4   1     0.00   0.00   0.49     0.00  -0.05  -0.63     0.03   0.00   0.00
+   5   1     0.00   0.00  -0.16     0.00  -0.31   0.00    -1.00   0.00   0.00
+   6  35     0.00   0.00  -0.16     0.00   0.02   0.00     0.01   0.00   0.00
+   ##SNIP##
+   10                     11                     12
+   A1                     B2                     A1
+   Frequencies --  2521.0940              3410.1755              3512.0957
+   Red. masses --     1.0211                 1.0848                 1.2333
+   Frc consts  --     3.8238                 7.4328                 8.9632
+   IR Inten    --   264.5877               109.0525                 0.0637
+   Atom AN      X      Y      Z        X      Y      Z        X      Y      Z
+   1   6     0.00   0.00   0.00     0.00   0.06   0.00     0.00  -0.10   0.00
+   2   6     0.00   0.00   0.00     0.00   0.06   0.00     0.00   0.10   0.00
+   3   1     0.00   0.01   0.00     0.00  -0.70   0.01     0.00   0.70  -0.01
+   4   1     0.00  -0.01   0.00     0.00  -0.70  -0.01     0.00  -0.70  -0.01
+   5   1     0.00   0.00   1.00     0.00   0.00   0.00     0.00   0.00   0.00
+   6  35     0.00   0.00  -0.01     0.00   0.00   0.00     0.00   0.00   0.00
+   
+   -------------------
+   - Thermochemistry -
+   -------------------
+   */
+  
   /**
    * Interprets the Harmonic frequencies section.
    *
@@ -301,9 +318,9 @@ class GaussianReader extends AtomSetCollectionReader {
     String line;
     String[] tokens; String[] symmetries; String[] frequencies;
     String[] red_masses; String[] frc_consts; String[] intensities;
- 
+    
     while ((line = reader.readLine()) != null &&
-            line.indexOf(":")<0) {
+        line.indexOf(":")<0) {
     }
     if (line == null)
       throw (new Exception("No frequencies encountered"));
@@ -311,40 +328,46 @@ class GaussianReader extends AtomSetCollectionReader {
     // G98 ends the frequencies with a line with a space (03 an empty line)
     // so I decided to read till the line is too short
     while ((line= reader.readLine()) != null &&
-           line.length() > 15)
+        line.length() > 15)
     {
       // we now have the line with the vibration numbers in them, but don't need it
       symmetries = getTokens(reader.readLine()); // read symmetry labels
-      // read only the 4 properties that always should be there...
+      // TODO I should really read all the properties of the vibrations listed
+      // and not limit myself to only IR type ones..
       frequencies = getTokens(discardLinesUntilStartsWith(reader," Frequencies"), 15);
       red_masses = getTokens(discardLinesUntilStartsWith(reader," Red. masses"), 15);
       frc_consts = getTokens(discardLinesUntilStartsWith(reader," Frc consts"), 15);
       intensities = getTokens(discardLinesUntilStartsWith(reader," IR Inten"), 15);
-
+      
       int frequencyCount = frequencies.length;
-
+      
       for (int i = 0; i < frequencyCount; ++i) {
         atomSetCollection.cloneLastAtomSet();
         atomSetCollection.setAtomSetName(
-           symmetries[i] + " " +
-           frequencies[i]+" cm-1, Inten = " +
-           intensities[i] + " KM/Mole " +
-           scfKey + " = " + scfEnergy
+            symmetries[i] + " "
+            + frequencies[i]+" cm**-1"
+//            + ", Inten = " + intensities[i] + " KM/Mole "
+//            + scfKey + " = " + scfEnergy
         );
         // set the properties
         atomSetCollection.setAtomSetProperty(scfKey, scfEnergy);
-        atomSetCollection.setAtomSetProperty("Frequency",frequencies[i]+" cm**-1");
-        atomSetCollection.setAtomSetProperty("Reduced Mass",red_masses[i]+" AMU");
-        atomSetCollection.setAtomSetProperty("Force Constant",frc_consts[i]+" mDyne/A");
-        atomSetCollection.setAtomSetProperty("IR Intensity",intensities[i]+" KM/Mole");
+        atomSetCollection.setAtomSetProperty("Frequency",
+            frequencies[i]+" cm**-1");
+        atomSetCollection.setAtomSetProperty("Reduced Mass",
+            red_masses[i]+" AMU");
+        atomSetCollection.setAtomSetProperty("Force Constant",
+            frc_consts[i]+" mDyne/A");
+        atomSetCollection.setAtomSetProperty("IR Intensity",
+            intensities[i]+" KM/Mole");
         atomSetCollection.setAtomSetProperty(SmarterJmolAdapter.PATH_KEY,
-          calculationNumber+SmarterJmolAdapter.PATH_SEPARATOR+"Frequencies");
+            "Calculation " + calculationNumber+
+            SmarterJmolAdapter.PATH_SEPARATOR+"Frequencies");
       }
       
       int atomCount = atomSetCollection.getLastAtomSetAtomCount();
       int firstModelAtom =
         atomSetCollection.atomCount - frequencyCount * atomCount;
-
+      
       // position to start reading the displacement vectors
       discardLinesUntilStartsWith(reader, " Atom AN");
       
@@ -353,7 +376,7 @@ class GaussianReader extends AtomSetCollectionReader {
         tokens = getTokens(reader.readLine());
         int atomCenterNumber = parseInt(tokens[0]);
         for (int j = 0, offset=FREQ_FIRST_VECTOR_OFFSET;
-             j < frequencyCount; ++j) {
+        j < frequencyCount; ++j) {
           int atomOffset = firstModelAtom+j*atomCount + atomCenterNumber - 1 ;
           Atom atom = atomSetCollection.atoms[atomOffset];
           atom.vectorX = parseFloat(tokens[offset++]);
@@ -363,28 +386,30 @@ class GaussianReader extends AtomSetCollectionReader {
       }
     }
   }
-
-/* SAMPLE Mulliken Charges OUTPUT from G98 */
-/*
- Mulliken atomic charges:
-              1
-     1  C   -0.238024
-     2  C   -0.238024
- ###SNIP###
-     6  Br  -0.080946
- Sum of Mulliken charges=   0.00000
-*/
   
-/**
- * Reads partial charges and assigns them only to the last atom set. 
- * @param reader The reader from which to read the charges
- * @throws Exception When an I/O error or discardlines error occurs
- */
-void readPartialCharges(BufferedReader reader) throws Exception {
+  /* SAMPLE Mulliken Charges OUTPUT from G98 */
+  /*
+   Mulliken atomic charges:
+   1
+   1  C   -0.238024
+   2  C   -0.238024
+   ###SNIP###
+   6  Br  -0.080946
+   Sum of Mulliken charges=   0.00000
+   */
+  
+  /**
+   * Reads partial charges and assigns them only to the last atom set. 
+   * @param reader The reader from which to read the charges
+   * @throws Exception When an I/O error or discardlines error occurs
+   */
+  // TODO this really should set the charges for the last nOrientations read
+  // being careful about the dummy atoms...
+  void readPartialCharges(BufferedReader reader) throws Exception {
     discardLines(reader, 1);
     for (int i = atomSetCollection.getLastAtomSetAtomIndex();
-         i < atomSetCollection.atomCount;
-         ++i) {
+    i < atomSetCollection.atomCount;
+    ++i) {
       // first skip over the dummy atoms
       while (atomSetCollection.atoms[i].elementNumber == 0)
         ++i;
@@ -393,5 +418,4 @@ void readPartialCharges(BufferedReader reader) throws Exception {
         parseFloat(getTokens(reader.readLine())[2]);
     }
   }
-  
 }

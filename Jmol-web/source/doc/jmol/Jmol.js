9b3366128c63530bdb806dcfe961015957abf3a1
Miguel <migueljmol@users.sourceforge.net>
2004-11-28 04:36:31 +0000

    new Jmol.js for ie mac

--- Jmol-web/source/doc/jmol/Jmol.js
+++ Jmol-web/source/doc/jmol/Jmol.js
@@ -227,7 +227,6 @@ function jmolSetTarget(targetSuffix) {
 }
 
 function jmolScript(script, targetSuffix) {
-  alert('jmolScript');
   if (script) {
     _jmolCheckBrowser();
     var target = "jmolApplet" + (targetSuffix ? targetSuffix : "0");
@@ -523,7 +522,6 @@ function _jmolInitCheck() {
 }
 
 function _jmolCheckBrowser() {
-  alert('_jmolCheckBrowser()');
   with (_jmol) {
     if (browserChecked)
       return;
@@ -551,7 +549,7 @@ function _jmolCheckBrowser() {
 }
 
 function _jmolPopup(url) {
-  var popup = window.open(url, "Jmol Popup",
+  var popup = window.open(url, "JmolPopup",
                           "left=150,top=150,height=400,width=600," +
                           "directories=yes,location=yes,menubar=yes," +
                           "toolbar=yes," +
@@ -600,9 +598,10 @@ function _jmolRadio(script, labelHtml, isChecked, separatorHtml, groupName) {
     separatorHtml = "";
   var scriptIndex = _jmolAddScript(script);
   return "<input name='" + groupName +
-         "' type='radio' onClick='alert(\'HI\');_jmolClick(" + scriptIndex +
-         _jmol.targetText +
-         "); return true;' onMouseover='_jmolMouseOver(" + scriptIndex +
+         "' type='radio' onClick='_jmolClick(" +
+         scriptIndex + _jmol.targetText +
+         ");return true;' onMouseover='_jmolMouseOver(" +
+         scriptIndex +
          ");return true;' onMouseout='_jmolMouseOut()' " +
 	 (isChecked ? "checked " : "") + _jmol.radioCssText + "/>" +
          labelHtml + separatorHtml;

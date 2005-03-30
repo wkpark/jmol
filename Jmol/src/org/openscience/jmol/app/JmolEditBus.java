c6a5b20d4f790b7879ffb003ed1bb23507461de7
Egon Willighagen <egonw@users.sourceforge.net>
2005-03-30 12:06:03 +0000

    Completed the implementations of getChemModel and getChemFile

--- Jmol/src/org/openscience/jmol/app/JmolEditBus.java
+++ Jmol/src/org/openscience/jmol/app/JmolEditBus.java
@@ -1,35 +1,31 @@
 /* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
- *
- * Copyright (C) 2003-2005  The Jmol Development Team
- *
- * Contact: jmol-developers@lists.sf.net
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307  USA.
- */
+* $Author$
+* $Date$
+* $Revision$
+*
+* Copyright (C) 2003-2005  The Jmol Development Team
+*
+* Contact: jmol-developers@lists.sf.net
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+*  02111-1307  USA.
+*/
 package org.openscience.jmol.app;
 
 import org.openscience.cdk.applications.plugin.CDKEditBus;
-import org.openscience.cdk.ChemFile;
-import org.openscience.cdk.ChemModel;
-import org.openscience.cdk.ChemSequence;
-import org.openscience.cdk.Atom;
-import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.*;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.geometry.GeometryTools;
 import org.openscience.cdk.geometry.CrystalGeometryTools;
@@ -41,8 +37,10 @@ import org.jmol.api.JmolViewer;
 import java.io.IOException;
 import java.io.Reader;
 
-public class JmolEditBus implements CDKEditBus {
+import javax.vecmath.*;
 
+public class JmolEditBus implements CDKEditBus {
+    
     private final static String APIVersion = "1.8";
     
     private JmolViewer viewer = null;
@@ -58,7 +56,7 @@ public class JmolEditBus implements CDKEditBus {
     public void showChemFile(Reader file) {
         viewer.openReader("", "", file);
     }
-
+    
     public void showChemFile(ChemFile file) {
         AtomContainer atomContainer = ChemFileManipulator.getAllInOneContainer(file);
         Atom[] atoms = atomContainer.getAtoms();
@@ -69,7 +67,7 @@ public class JmolEditBus implements CDKEditBus {
         }
         // check wether there are 3D coordinates
         if (!GeometryTools.has3DCoordinates(atomContainer) &&
-            !CrystalGeometryTools.hasCrystalCoordinates(atomContainer)) {
+        !CrystalGeometryTools.hasCrystalCoordinates(atomContainer)) {
             System.err.println("Cannot display chemistry without 3D coordinates");
             return;
         }
@@ -101,24 +99,49 @@ public class JmolEditBus implements CDKEditBus {
         file.addChemSequence(sequence);
         showChemFile(file);
     }
-
+    
     public ChemModel getChemModel() {
-        if (viewer.getModelCount() < 0) {
+        if (viewer.getModelCount() != 1) {
+            // cannot deal with no models, or more than one model
             return null;
         } else {
             ChemModel model = new ChemModel();
             SetOfMolecules moleculeSet = new SetOfMolecules();
             Molecule molecule = new Molecule();
             // copy the atoms
-            for (int i=0; viewer.getAtomCount(); i++) {
+            Point3f[] atomPoints = new Point3f[viewer.getAtomCount()];
+            for (int i=0; i<viewer.getAtomCount(); i++) {
                 Atom atom = new Atom(viewer.getAtomName(i));
-                atom.setPoint3d(new Point3d(viewer.getAtomPoint3f(i)));
+                atomPoints[i] = viewer.getAtomPoint3f(i);
+                atom.setPoint3d(new Point3d(atomPoints[i]));
                 molecule.addAtom(atom);
             }
             // copy the bonds
-            for (int i=0; viewer.getBondCount(); i++) {
-                molecule.addBond();
+            for (int i=0; i<viewer.getBondCount(); i++) {
+                // Ok, at the time or writing the JmolViewer no longer has
+                // something like getBondAtomNumber1() :(
+                int atomNumber1 = -1;
+                int atomNumber2 = -1;
+                Point3f atomCoord = viewer.getBondPoint3f1(i);
+                for (int j=0; j<atomPoints.length; j++) {
+                    if (atomCoord.distance(atomPoints[j]) < 0.01) {
+                        atomNumber1 = j;
+                        j=atomPoints.length;
+                    }
+                }
+                atomCoord = viewer.getBondPoint3f2(i);
+                for (int j=0; j<atomPoints.length; j++) {
+                    if (atomCoord.distance(atomPoints[j]) < 0.01) {
+                        atomNumber2 = j;
+                        j=atomPoints.length;
+                    }
+                }
+                // just assume this is working ...
+                molecule.addBond(atomNumber1, atomNumber2, viewer.getBondOrder(i));
             }
+            moleculeSet.addMolecule(molecule);
+            model.setSetOfMolecules(moleculeSet);
+            return model;
         }
     }
     

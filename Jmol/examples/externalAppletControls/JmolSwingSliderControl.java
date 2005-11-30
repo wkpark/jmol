390f6752eeff35351038db6aa1e4898b03e32c88
Nicolas <nicove@users.sourceforge.net>
2005-11-30 21:09:00 +0000

    Reorganization of examples directory (examples becomes examples/basic)

--- Jmol/examples/externalAppletControls/JmolSwingSliderControl.java
+++ Jmol/examples/externalAppletControls/JmolSwingSliderControl.java
@@ -14,10 +14,8 @@
 
 import org.jmol.applet.JmolAppletRegistry;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
-import javax.swing.border.*;
 
 public class JmolSwingSliderControl
   extends JApplet implements ChangeListener {
@@ -37,7 +35,7 @@ public class JmolSwingSliderControl
 
     JPanel panel = new JPanel(new BorderLayout());
     panel.setBorder(BorderFactory.createRaisedBevelBorder());
-    panel.add(new JLabel("spacefill percentage", JLabel.CENTER),
+    panel.add(new JLabel("spacefill percentage", SwingConstants.CENTER),
               BorderLayout.NORTH);
     swingSlider = new JSlider(0, 100);
     swingSlider.addChangeListener(this);

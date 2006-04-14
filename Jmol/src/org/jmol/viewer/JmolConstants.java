b899374c3113154dfbe5dfcb5a3d0ea7500627da
Nicolas <nicove@users.sourceforge.net>
2006-04-14 18:06:13 +0000

    Release 10.2.0

--- Jmol/src/org/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/jmol/viewer/JmolConstants.java
@@ -30,7 +30,7 @@ final public class JmolConstants {
   // for now, just update this by hand
   // perhaps use ant filter later ... but mth doesn't like it :-(
   public final static String copyright = "(C) 2006 Jmol Development";
-  public final static String version = "10.00.60";
+  public final static String version = "10.2.0";
   public final static String cvsDate =
     "$Date$";
   public final static String date = cvsDate.substring(7, 23);

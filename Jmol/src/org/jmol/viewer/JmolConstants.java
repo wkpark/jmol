0aca6c5fea66637989217f9c186d5dea76a32710
Bob Hanson <hansonr@stolaf.edu>
2006-09-14 12:39:52 +0000

    10.9.59  stereo color1 [optional color2]

--- Jmol/src/org/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/jmol/viewer/JmolConstants.java
@@ -42,7 +42,7 @@ final public class JmolConstants {
   // for now, just update this by hand
   // perhaps use ant filter later ... but mth doesn't like it :-(
   public final static String copyright = "(C) 2006 Jmol Development";
-  public final static String version = "10.9.58";
+  public final static String version = "10.9.59";
   public final static String cvsDate = "$Date$";
   public final static String date = cvsDate.substring(7, 23);
   

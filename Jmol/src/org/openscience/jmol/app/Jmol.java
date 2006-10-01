027d103085fb78b2a8fbcbf74d6cdbbc84ee03f5
Nicolas <nicove@users.sourceforge.net>
2006-10-01 06:39:20 +0000

    Logger parameters on the command line

--- Jmol/src/org/openscience/jmol/app/Jmol.java
+++ Jmol/src/org/openscience/jmol/app/Jmol.java
@@ -446,6 +446,12 @@ public class Jmol extends JPanel {
         System.out.println("  cdk.debug.stdout=[true|false] (false)");
         System.out.println("  display.speed=[fps|ms] (ms)");
         System.out.println("  JmolConsole=[true|false] (true)");
+        System.out.println("  jmol.logger.debug=[true|false] (false)");
+        System.out.println("  jmol.logger.error=[true|false] (false)");
+        System.out.println("  jmol.logger.fatal=[true|false] (false)");
+        System.out.println("  jmol.logger.info=[true|false] (true)");
+        System.out.println("  jmol.logger.logLevel=[true|false] (false)");
+        System.out.println("  jmol.logger.warn=[true|false] (false)");
         System.out.println("  plugin.dir (unset)");
         System.out.println("  user.language=[DE|EN|ES|FR|NL|PL] (EN)");
 

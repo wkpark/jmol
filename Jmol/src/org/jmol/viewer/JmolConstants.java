9daec9a1e9e2229ed96a5b185ac3cc4b50eeb283
Bob Hanson <hansonr@stolaf.edu>
2006-10-01 00:03:33 +0000

    10.9.70 ready
    -bug fix for SHELX reader not reading LATT record
    -bug fix for frame not unloading properly between models

--- Jmol/src/org/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/jmol/viewer/JmolConstants.java
@@ -43,7 +43,7 @@ final public class JmolConstants {
   // for now, just update this by hand
   // perhaps use ant filter later ... but mth doesn't like it :-(
   public final static String copyright = "(C) 2006 Jmol Development";
-  public final static String version = "10.9.69";
+  public final static String version = "10.9.70";
   public final static String cvsDate = "$Date$";
   public final static String date = cvsDate.substring(7, 23);
   

8a209e52157af0b9f99e9c91c65f33229394ef3d
Bob Hanson <hansonr@stolaf.edu>
2006-10-26 04:29:37 +0000

    10.9.85
    -move command zoom fixed
    -"o" for unitcell axes
    -3D positionable echos

--- Jmol/src/org/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/jmol/viewer/JmolConstants.java
@@ -43,7 +43,7 @@ final public class JmolConstants {
   // for now, just update this by hand
   // perhaps use ant filter later ... but mth doesn't like it :-(
   public final static String copyright = "(C) 2006 Jmol Development";
-  public final static String version = "10.9.84"; //set labelPointer business; minor bug fixes - polyhedra restrict;  
+  public final static String version = "10.9.85"; //3D labels; 0 origin for unitcell; move command zoom fix  
   public final static String cvsDate = "$Date$";
   public final static String date = cvsDate.substring(7, 23);
   

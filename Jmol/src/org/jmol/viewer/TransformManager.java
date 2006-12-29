7ba219bd9a287040fb6d5c85ef9aa8d652fb08b1
Nicolas <nicove@users.sourceforge.net>
2006-12-29 17:52:29 +0000

    Fixing warning

--- Jmol/src/org/jmol/viewer/TransformManager.java
+++ Jmol/src/org/jmol/viewer/TransformManager.java
@@ -1729,7 +1729,7 @@ abstract class TransformManager {
     vibrationScale = 0;
   }
 
-  private class VibrationThread extends Thread implements Runnable {
+  /*private*/ class VibrationThread extends Thread implements Runnable {
 
     public void run() {
       long startTime = System.currentTimeMillis();

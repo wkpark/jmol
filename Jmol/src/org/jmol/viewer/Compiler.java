16c109f2efaaa377eca472d9520642f6eb189804
Bob Hanson <hansonr@stolaf.edu>
2007-02-08 04:44:12 +0000

    frame/compiler fix

--- Jmol/src/org/jmol/viewer/Compiler.java
+++ Jmol/src/org/jmol/viewer/Compiler.java
@@ -1113,8 +1113,7 @@ class Compiler {
     case Token.substructure:
       return clauseSubstructure();
     case Token.decimal:
-      addTokenToPostfix(new Token(Token.all));
-      return generateResidueSpecCode(new Token(Token.spec_model, getToken().intValue, null));
+      return addTokenToPostfix(new Token(Token.spec_model, getToken().intValue, null));
     case Token.hyphen: // selecting a negative residue spec
     case Token.integer:
     case Token.seqcode:

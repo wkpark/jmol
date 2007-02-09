0dff89df4f488f6b46464ee52666e5767c289f1b
Bob Hanson <hansonr@stolaf.edu>
2007-02-09 21:06:37 +0000

    11.1.10

--- Jmol/src/org/jmol/viewer/JmolConstants.java
+++ Jmol/src/org/jmol/viewer/JmolConstants.java
@@ -34,7 +34,7 @@ final public class JmolConstants {
   // for now, just update this by hand
   // perhaps use ant filter later ... but mth doesn't like it :-(
   public final static String copyright = "(C) 2006 Jmol Development";
-  public final static String version = "11.1.9"; //select 2.3  #file.model     
+  public final static String version = "11.1.10"; //script check/line/command; translucent fix     
   public final static String cvsDate = "$Date$";
   public final static String date = cvsDate.substring(7, 23);
     

851e898cdebfc49b76a2a7740529d2d91f89025a
Bob Hanson <hansonr@stolaf.edu>
2007-02-10 20:49:15 +0000

    11.1 _memory

--- Jmol/src/org/openscience/jmol/app/DisplayPanel.java
+++ Jmol/src/org/openscience/jmol/app/DisplayPanel.java
@@ -512,10 +512,7 @@ public class DisplayPanel extends JPanel
     if (displaySpeed.equalsIgnoreCase("fps")) {
         status.setStatus(3, fmt(1000/timeLast) + "FPS : " + fmt(1000/timeAverage) + "FPS");
     } else {
-      Runtime runtime = Runtime.getRuntime();
-      float bTotal = ((int)(runtime.totalMemory() / 100000))/10f;
-      float bFree = ((int) (runtime.freeMemory()/100000))/10f;
-      status.setStatus(3, timeLast + "ms," + timeAverage + "ms;Mb:" + (bTotal-bFree) + "/" + bTotal);
+      status.setStatus(3, timeLast + "ms," + timeAverage + "ms;Mb:" + viewer.getParameter("_memory"));
     }
   }
 }

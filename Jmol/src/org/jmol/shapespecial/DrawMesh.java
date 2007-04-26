284a9c619324553f14ec6c6100870580bdf85ee2
Bob Hanson <hansonr@stolaf.edu>
2007-04-26 21:51:54 +0000

    11.1.30 refactored shapespecial package

---
+++ Jmol/src/org/jmol/shapespecial/DrawMesh.java
@@ -0,0 +1,112 @@
+/* $RCSfile$
+ * $Author: hansonr $
+ * $Date: 2007-04-14 23:28:16 -0500 (Sat, 14 Apr 2007) $
+ * $Revision: 7408 $
+ *
+ * Copyright (C) 2005  Miguel, The Jmol Development Team
+ *
+ * Contact: miguel@jmol.org, jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jmol.shapespecial;
+
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+
+import org.jmol.g3d.Graphics3D;
+
+public class DrawMesh extends Mesh {
+  
+  DrawMesh(String thisID, Graphics3D g3d, short colix) {
+    super(thisID, g3d, colix);
+  }
+
+  final static int DRAW_MULTIPLE = -1;
+  final static int DRAW_NONE = 0;
+  final static int DRAW_ARROW = 1;
+  final static int DRAW_CIRCLE = 2;
+  final static int DRAW_CURVE = 3;
+  final static int DRAW_LINE = 4;
+  final static int DRAW_PLANE = 5;
+  final static int DRAW_POINT = 6;
+  final static int DRAW_TRIANGLE = 7;
+  final static int ISOSURFACE_BICOLOR = 8;
+  
+  int drawType = DRAW_TRIANGLE;
+  int[] drawTypes;
+  Point3f ptCenters[];
+  Vector3f axis = new Vector3f(1,0,0);
+  Vector3f axes[];
+  int drawVertexCount;
+  int[] drawVertexCounts;
+
+  String getDrawType() {
+    switch (drawType) {
+    case DRAW_MULTIPLE:
+      return "multiple";
+    case DRAW_ARROW:
+      return "arrow";
+    case DRAW_CIRCLE:
+      return "circle";
+    case DRAW_CURVE:
+      return "curve";
+    case DRAW_POINT:
+      return "point";
+    case DRAW_LINE:
+      return "line";
+    case DRAW_TRIANGLE:
+      return "triangle";
+    case DRAW_PLANE:
+      return "plane";
+    }
+    return "type is not identified in mesh.getDrawType()";
+  }
+
+  final void setCenter(int iModel) {
+    Point3f center = new Point3f(0, 0, 0);
+    int iptlast = -1;
+    int ipt = 0;
+    int n = 0;
+    for (int i = polygonCount; --i >= 0;) {
+      if (iModel >=0 && i != iModel)
+        continue;
+      iptlast = -1;
+      for (int iV = polygonIndexes[i].length; --iV >= 0;) {
+        ipt = polygonIndexes[i][iV];
+        if (ipt == iptlast)
+          continue;
+        iptlast = ipt;
+        center.add(vertices[ipt]);
+        n++;
+      }
+      if (n > 0 && (i == iModel || i == 0)) {
+        center.scale(1.0f / n);
+        break;
+      }
+    }
+    if (iModel < 0){
+      ptCenter = center;
+    } else {
+      ptCenters[iModel] = center;
+    }
+  }
+
+  void offset(Vector3f offset) {
+    for (int i = vertexCount; --i >= 0;)
+      vertices[i].add(offset);
+  }
+}
---
+++ Jmol/src/org/jmol/shapespecial/MeshRenderer.java
@@ -0,0 +1,159 @@
+/* $RCSfile$
+ * $Author: hansonr $
+ * $Date: 2007-04-25 09:53:35 -0500 (Wed, 25 Apr 2007) $
+ * $Revision: 7491 $
+ *
+ * Copyright (C) 2002-2005  The Jmol Development Team
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jmol.shapespecial;
+
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+import javax.vecmath.Point3i;
+import org.jmol.g3d.Graphics3D;
+import org.jmol.shape.ShapeRenderer;
+
+public abstract class MeshRenderer extends ShapeRenderer {
+
+  Point3f[] vertices;
+  short[] normixes;
+  Point3i[] screens;
+  Vector3f[] transformedVectors;
+  int vertexCount;
+  int lighting;
+  boolean frontOnly;
+  
+  Mesh mesh;
+
+  protected void render() {
+    MeshCollection mc = (MeshCollection) shape;
+    for (int i = mc.meshCount; --i >= 0;)
+      render1(mc.meshes[i]);
+  }
+
+  public boolean render1(Mesh mesh) {  //used by mps renderer
+    this.mesh = mesh;
+    if (!setVariables())
+      return false;
+    transform();
+    render2();
+    viewer.freeTempScreens(screens);
+    return true;
+  }
+  
+  private boolean setVariables() {
+    if (mesh == null || mesh.visibilityFlags == 0 || !g3d.setColix(mesh.colix)
+        || (vertexCount = mesh.vertexCount) == 0)
+      return false;
+    vertices = mesh.vertices;
+    normixes = mesh.normixes;
+    if (normixes == null || vertices == null)
+      return false; 
+    //this can happen when user switches windows 
+    // during a surface calculation
+    lighting = mesh.lighting;
+    frontOnly = mesh.frontOnly && !mesh.isTwoSided;
+    screens = viewer.allocTempScreens(vertexCount);
+    transformedVectors = g3d.getTransformedVertexVectors();
+    return true;
+  }
+
+  // all of the following methods are overridden in subclasses
+  // DO NOT change parameters without first checking for the
+  // same method in a subclass.
+  
+  void transform() {
+    for (int i = vertexCount; --i >= 0;)
+      viewer.transformPoint(vertices[i], screens[i]);
+  }
+  
+  boolean isPolygonDisplayable(int i) {
+    return true;
+  }
+
+  void render2() {
+    if (mesh.showPoints)
+      renderPoints();
+    if (mesh.drawTriangles)
+      renderTriangles(false, false);
+    if (mesh.fillTriangles)
+      renderTriangles(true, mesh.showTriangles);
+  }
+  
+  void renderPoints() {
+    for (int i = vertexCount; --i >= 0;)
+      if (!frontOnly || transformedVectors[normixes[i]].z >= 0)
+        g3d.fillSphereCentered(4, screens[i]);
+  }
+
+  void renderTriangles(boolean fill, boolean iShowTriangles) {
+    int[][] polygonIndexes = mesh.polygonIndexes;
+    short colix = mesh.colix;
+    //vertexColixes are only isosurface properties of IsosurfaceMesh, not Mesh
+    g3d.setColix(colix);
+    for (int i = mesh.polygonCount; --i >= 0;) {
+      if (!isPolygonDisplayable(i))
+        continue;
+      int[] vertexIndexes = polygonIndexes[i];
+      int iA = vertexIndexes[0];
+      int iB = vertexIndexes[1];
+      int iC = vertexIndexes[2];
+      if (iB == iC) {
+        int diameter = (mesh.diameter > 0 ? mesh.diameter : iA == iB ? 6 : 3);
+        g3d.fillCylinder(Graphics3D.ENDCAPS_SPHERICAL, diameter, screens[iA],
+            screens[iB]);
+        continue;
+      }
+      switch (vertexIndexes.length) {
+      case 3:
+        if (frontOnly && transformedVectors[normixes[iA]].z < 0
+            && transformedVectors[normixes[iB]].z < 0
+            && transformedVectors[normixes[iC]].z < 0)
+          continue;
+        if (fill) {
+          if (iShowTriangles) {
+            g3d.fillTriangle(screens[iA], colix, normixes[iA], screens[iB],
+                colix, normixes[iB], screens[iC], colix, normixes[iC], 0.1f);
+            continue;
+          }
+          g3d.fillTriangle(screens[iA], colix, normixes[iA], screens[iB],
+              colix, normixes[iB], screens[iC], colix, normixes[iC]);
+          continue;
+        }
+        g3d.drawTriangle(screens[iA], screens[iB], screens[iC], 7);
+        continue;
+      case 4:
+        int iD = vertexIndexes[3];
+        if (frontOnly && transformedVectors[normixes[iA]].z < 0
+            && transformedVectors[normixes[iB]].z < 0
+            && transformedVectors[normixes[iC]].z < 0
+            && transformedVectors[normixes[iD]].z < 0)
+          continue;
+        if (fill) {
+          g3d.fillQuadrilateral(screens[iA], colix, normixes[iA], screens[iB],
+              colix, normixes[iB], screens[iC], colix, normixes[iC],
+              screens[iD], colix, normixes[iD]);
+          continue;
+        }
+        g3d.drawQuadrilateral(colix, screens[iA], screens[iB], screens[iC],
+            screens[iD]);
+      }
+    }
+  }  
+}

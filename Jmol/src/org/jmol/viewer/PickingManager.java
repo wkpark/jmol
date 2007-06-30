5afc7a44ac291571c21c71d65d67e928bd488ef8
Bob Hanson <hansonr@stolaf.edu>
2007-06-30 01:09:44 +0000

    11.3.2  bug fix: set picking label

--- Jmol/src/org/jmol/viewer/PickingManager.java
+++ Jmol/src/org/jmol/viewer/PickingManager.java
@@ -123,7 +123,7 @@ class PickingManager {
         toggleMeasurement(4);
       break;
     case JmolConstants.PICKING_LABEL:
-      viewer.script("toggleLabel = (atomindex="+atomIndex+")");
+      viewer.script("toggleLabel = {atomindex="+atomIndex+"}");
       break;
     case JmolConstants.PICKING_CENTER:
       viewer.script("zoomTo (atomindex=" + atomIndex+")");

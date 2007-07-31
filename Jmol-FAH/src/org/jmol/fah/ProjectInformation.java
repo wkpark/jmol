02d97f279938549f67abfd16c8de3f0673bab13e
Nicolas <nicove@users.sourceforge.net>
2007-07-31 10:09:23 +0000

    Folding@Home

--- Jmol-FAH/src/org/jmol/fah/ProjectInformation.java
+++ Jmol-FAH/src/org/jmol/fah/ProjectInformation.java
@@ -603,6 +603,8 @@ public class ProjectInformation {
       if (core != null) {
         if ("AMBER".equalsIgnoreCase(core)) {
           info._fciCore = CoreType.AMBER;
+        } else if ("DB".equalsIgnoreCase(core)) {
+          info._fciCore = CoreType.DGROMACSB;
         } else if ("DGROMACS".equalsIgnoreCase(core)) {
           info._fciCore = CoreType.DGROMACS;
         } else if ("GBGROMACS".equalsIgnoreCase(core)) {

45903a135754a2bbe0308b60ab5ba3ab2d2aaf81
Nicolas <nicove@users.sourceforge.net>
2008-03-15 13:23:46 +0000

    Folding@Home

--- Jmol-FAH/src/org/jmol/fah/ProjectInformation.java
+++ Jmol-FAH/src/org/jmol/fah/ProjectInformation.java
@@ -609,6 +609,8 @@ public class ProjectInformation {
           info._fciCore = CoreType.DGROMACS;
         } else if ("GBGROMACS".equalsIgnoreCase(core)) {
           info._fciCore = CoreType.GBGROMACS;
+        } else if ("GC".equalsIgnoreCase(core)) {
+          info._fciCore = CoreType.GROCVS;
         } else if ("GROGPU".equalsIgnoreCase(core)) {
           info._fciCore = CoreType.GROGPU;
         } else if ("GROMACS".equalsIgnoreCase(core)) {

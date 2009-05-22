0376f26d3e55ea7b298edc35335a89adad1fd93c
Bob Hanson <hansonr@stolaf.edu>
2009-05-22 20:36:09 +0000

    11.7.38  VERSION # spartan smol reader broken in 11.7.37
    oops -- sorry about that!

--- Jmol/src/org/jmol/adapter/smarter/AtomSetCollectionReader.java
+++ Jmol/src/org/jmol/adapter/smarter/AtomSetCollectionReader.java
@@ -550,7 +550,7 @@ public abstract class AtomSetCollectionReader {
   }
 
   private Matrix3f matrixRotate;
-  protected void setTransform(float x1, float y1, float z1, float x2, float y2,
+  public void setTransform(float x1, float y1, float z1, float x2, float y2,
                               float z2, float x3, float y3, float z3) {
     if (matrixRotate != null)
       return;

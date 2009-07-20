79066531d8991a5d26f08c596e5f204331e0ae1d
Bob Hanson <hansonr@stolaf.edu>
2009-07-20 03:34:22 +0000

    removing U3d exporter -- we can add that later some day;

--- Jmol/src/org/jmol/viewer/ScriptEvaluator.java
+++ Jmol/src/org/jmol/viewer/ScriptEvaluator.java
@@ -10489,7 +10489,7 @@ class ScriptEvaluator {
           ERROR_writeWhat,
           "COORDS|FILE|FUNCTIONS|HISTORY|IMAGE|ISOSURFACE|MENU|MO|POINTGROUP|QUATERNION [w,x,y,z] [derivative]"
               + "|RAMACHANDRAN|STATE|VAR x  CLIPBOARD",
-          "JPG|JPG64|PNG|GIF|PPM|SPT|JVXL|XYZ|MOL|PDB|"
+          "JPG|JPG64|PNG|GIF|PPM|SPT|JVXL|XYZ|MOL|PDB|MAYA|POVRAY|VRML|X3D"
               + driverList.toUpperCase().replace(';', '|'));
     if (isSyntaxCheck)
       return "";

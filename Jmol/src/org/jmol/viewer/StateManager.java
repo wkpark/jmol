3e83d3bc30bf40aa8669e1435fc9322e78eabceb
Bob Hanson <hansonr@stolaf.edu>
2010-01-27 21:22:05 +0000

    version=11.9.23_dev # new feature: all saved orientations now saved in state

--- Jmol/src/org/jmol/viewer/StateManager.java
+++ Jmol/src/org/jmol/viewer/StateManager.java
@@ -385,7 +385,7 @@ public class StateManager {
     }
 
     public String getMoveToText(boolean asCommand) {
-      return (asCommand ? " " + moveToText + "\n save orientation \"" 
+      return (asCommand ? "  " + moveToText + "\n  save orientation \"" 
           + saveName.substring(12) + "\"\n" : moveToText);
     }
     

688d8a79653f27b165e8da2cefaf92156a2456f5
Bob Hanson <hansonr@stolaf.edu>
2010-02-19 15:41:51 +0000

    preliminary CRYSTAL reader - still needs testing

--- Jmol/src/org/jmol/adapter/readers/xtal/CrystalReader.java
+++ Jmol/src/org/jmol/adapter/readers/xtal/CrystalReader.java
@@ -113,7 +113,7 @@ public class CrystalReader extends AtomSetCollectionReader {
   }
 
   private void readCellParams() throws Exception {
-    if (isPolymer) {
+    if (isPolymer && !isPrimitive) {
       float a = parseFloat(line.substring(line.indexOf("CELL") + 4));
       setUnitCell(a, 1, 1, 90, 90, 90);
       return;
@@ -122,6 +122,9 @@ public class CrystalReader extends AtomSetCollectionReader {
     String[] tokens = getTokens(readLine());
     if (isSlab) {
       setUnitCell(parseFloat(tokens[0]), parseFloat(tokens[1]), 1,
+          parseFloat(tokens[3]), parseFloat(tokens[4]), parseFloat(tokens[5]));
+    } else if (isPolymer) {
+      setUnitCell(parseFloat(tokens[0]), 1, 1,
           parseFloat(tokens[3]), parseFloat(tokens[4]), parseFloat(tokens[5]));
     } else {
       setUnitCell(parseFloat(tokens[0]), parseFloat(tokens[1]), parseFloat(tokens[2]),

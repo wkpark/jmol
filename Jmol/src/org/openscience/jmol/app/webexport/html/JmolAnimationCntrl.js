8fcf0af21c34bb8611f15d2ee5ddafb97eb7720a
Jonathan Gutow <gutow@users.sourceforge.net>
2010-06-12 20:36:27 +0000

    somehow JmolAnimationCntrl.js didn't get committed when widgets were added to WebExport. This corrects that.

---
+++ Jmol/src/org/openscience/jmol/app/webexport/html/JmolAnimationCntrl.js
@@ -0,0 +1,30 @@
+/*jmolAnimationCntrl.js
+
+J. Gutow May 2010
+
+This includes one function that controls the highlighting of the
+animation mode buttons
+*/
+
+function jmol_animationmode(selected, n){
+    var cellID = "jmol_loop_"+n;
+    document.getElementById(cellID).removeAttribute("style");
+    cellID = "jmol_playOnce_"+n;
+    document.getElementById(cellID).removeAttribute("style");
+    cellID = "jmol_palindrome_"+n;
+    document.getElementById(cellID).removeAttribute("style");
+    if (selected=="loop") {
+        cellID = "jmol_loop_"+n;
+        document.getElementById(cellID).setAttribute("style","background: blue;");
+        var result  = jmolScript('animation mode loop', n);
+    }
+    if (selected=="playOnce") {
+        cellID = "jmol_playOnce_"+n;
+        document.getElementById(cellID).setAttribute("style","background: blue;");
+        var result  = jmolScript('animation mode once', n);
+    }
+    if (selected=="palindrome") {
+        cellID = "jmol_palindrome_"+n;
+        document.getElementById(cellID).setAttribute("style","background: blue;");
+        var result  = jmolScript('animation mode palindrome',n);
+    }

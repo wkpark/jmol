dfea9e8acc9fb288e0a5c21e12dd7081fdfd5ce8
Angel Herraez <aherraez@users.sourceforge.net>
2010-10-17 17:12:10 +0000

    adding a "Reload this File" entry to top menubar (calls load "")

--- Jmol/src/org/openscience/jmol/app/jmolpanel/GuiMap.java
+++ Jmol/src/org/openscience/jmol/app/jmolpanel/GuiMap.java
@@ -61,6 +61,7 @@ class GuiMap {
     labels.put("openurl", GT._("Open &URL"));
     labels.put("openpdb", GT._("&Get PDB"));
     labels.put("openmol", GT._("Get &MOL"));
+    labels.put("reloadScript", GT._("&Reload this File"));
     labels.put("editor", GT._("Scrip&t Editor..."));  // new %t 11.7.45
     labels.put("console", GT._("Conso&le..."));
     labels.put("jconsole", GT._("Jmol Java &Console"));

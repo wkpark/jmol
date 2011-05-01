139e7da9c1ca226eff34d6a7ad342ae0b0fbe6d9
Bob Hanson <hansonr@stolaf.edu>
2011-05-01 20:16:53 +0000

    version=12.1.45_dev
    # bug fix: AIMS reader upgrade to include atom/atom_frac line mix
    
    
    
    Former-commit-id: 13754632de9e55d6f3f3e724cbc8d76ddde88343

--- Jmol/src/org/jmol/adapter/readers/xtal/AimsReader.java
+++ Jmol/src/org/jmol/adapter/readers/xtal/AimsReader.java
@@ -9,6 +9,10 @@
  *
  * Contact: meyer@fhi-berlin.mpg.de
  *
+ * Copyright (C) 2011  Joerg Meyer, TU Muenchen 
+ *
+ * Contact: joerg.meyer@ch.tum.de
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
@@ -27,92 +31,55 @@
 
 package org.jmol.adapter.readers.xtal;
 
-import org.jmol.adapter.smarter.*;
+import org.jmol.adapter.smarter.Atom;
+import org.jmol.adapter.smarter.AtomSetCollectionReader;
 import org.jmol.util.Logger;
-
-import javax.vecmath.Vector3f;
 
 /**
  * FHI-aims (http://www.fhi-berlin.mpg.de/aims) geometry.in file format
  *
  * samples of relevant lines in geometry.in file are included as comments below
+ * 
+ * modified (May 1, 2011, hansonr@stolaf.edu) to account for atom/atom_frac lines
+ * and to bring it into compliance with other load options (such as overriding
+ * file-based symmetry or unit cell parameters).
  *
- * @author Joerg Meyer, FHI Berlin 2009 (meyer@fhi-berlin.mpg.de)
- * @version 1.2
+ * @author Joerg Meyer, TU Muenchen 2011 (joerg.meyer@ch.tum.de)
+ * @version 1.3
  * 
  */
 
 public class AimsReader extends AtomSetCollectionReader {
 
-  private int nLatticeVectors = 0;
-  private Vector3f[] latticeVectors = new Vector3f[3];
-
+  private boolean globalDoApplySymmetry;
+  private boolean isFractional;
+  
+  @Override
+  protected void initializeReader() {
+    globalDoApplySymmetry = doApplySymmetry;
+    doApplySymmetry = true;
+    isFractional = true;
+  }
+  
   @Override
   protected boolean checkLine() {
     String[] tokens = getTokens();
-    float x, y, z;
     if (tokens.length == 0)
-      return true;
-    /*
-     * atom 2.750645380 2.750645380 25.000000000 Pd
-     */
-    if (tokens[0].equals("atom")) {
-      if (tokens.length < 5) {
-        Logger.warn("cannot read line with FHI-aims atom data: " + line);
-      } else {
-        x = parseFloat(tokens[1]);
-        y = parseFloat(tokens[2]);
-        z = parseFloat(tokens[3]);
-        String symbol = tokens[4];
-        Atom atom = atomSetCollection.addNewAtom();
-        atom.set(x, y, z);
-        atom.elementSymbol = symbol;
-      }
-      return true;
-    }
-
-    /*
-     * multipole 2.750645380 2.750645380 25.000000000 0 46.0
-     */
-    if (tokens[0].equals("multipole")) {
-      if (tokens.length < 6) {
-        Logger.warn("cannot read line with FHI-aims atom data: " + line);
-      } else {
-        x = parseFloat(tokens[1]);
-        y = parseFloat(tokens[2]);
-        z = parseFloat(tokens[3]);
-        int order = parseInt(tokens[4]);
-        float charge = parseFloat(tokens[5]);
-        if (order > 0) {
-          Logger
-              .warn("multipole line ignored since only monopoles are currently supported: "
-                  + line);
-          return true;
-        }
-        Atom atom = atomSetCollection.addNewAtom();
-        atom.set(x, y, z);
-        atom.partialCharge = charge;
-        atom.formalCharge = Math.round(charge);
-      }
-      return true;
-    }
-
-    /*
-     * lattice_vector 16.503872273 0.000000000 0.000000000
-     */
+      return true;    
     if (tokens[0].equals("lattice_vector")) {
-      if (tokens.length < 4) {
-        Logger.warn("cannot read line with FHI-aims lattice vector: " + line);
-      } else if (nLatticeVectors > 2) {
-        Logger.warn("more than 3 FHI-aims lattice vectors found with line: "
-            + line);
-      } else {
-        x = parseFloat(tokens[1]);
-        y = parseFloat(tokens[2]);
-        z = parseFloat(tokens[3]);
-        latticeVectors[nLatticeVectors] = new Vector3f(x, y, z);
-      }
-      nLatticeVectors++;
+      readLatticeVector(tokens);
+      return true;
+    }    
+    if (tokens[0].equals("atom")) {
+      readAtom(tokens, false);
+      return true;
+    }
+    if (tokens[0].equals("atom_frac")) {
+      readAtom(tokens, true);
+      return true;
+    }
+    if (tokens[0].equals("multipole")) {
+      readMultipole(tokens);
       return true;
     }
     return true;
@@ -120,67 +87,77 @@ public class AimsReader extends AtomSetCollectionReader {
 
   @Override
   protected void finalizeReader() throws Exception {
-    /*
-     * translational symmetry in less than three directions is currently neither
-     * supported by Jmol nor FHI-aims
-     */
-
-    if (nLatticeVectors == 3) {
-
-      /*
-       * enforce application of (translational!) symmetry in Jmol (in case of
-       * crystal lattice being present in the FHI-aims input file)
-       * 
-       * coordinates in FHI-aims input file are not fractional
-       * 
-       * transformation of lattice represenation (i.e. some linear algebra) is
-       * done in separate (recyclable) routine below
-       */
-
-      doApplySymmetry = true;
-      setFractionalCoordinates(false);
-      setUnitCellFromLatticeVectors(latticeVectors);
-
-      /*
-       * better do not hardcode as other interpreted to be given in file
-       * setSpaceGroupName("P1");
-       */
-
-      /*
-       * this takes care of properly converting cartesian coordinates to
-       * fractionals when necessary (i.e. use of symmetry is turned on) - and is
-       * crucial for unit cell being internally 'accepted' as such (as indicated
-       * in AtomSetCollectionReader.java) doing this above (when adding atoms)
-       * is not sufficient, as a unit cell has (deliberately!) not yet been
-       * added then
-       */
-      int nAtoms = atomSetCollection.getAtomCount();
-      for (int n = 0; n < nAtoms; n++) {
-        Atom atom = atomSetCollection.getAtom(n);
-        setAtomCoord(atom);
-      }
+    doApplySymmetry = globalDoApplySymmetry;
+    if (nLatticeVectors == 1 || nLatticeVectors == 2) {
+      Logger.warn("ignoring translation symmetry for more or less than 3 dimensions"
+                + "(which is currently neither supported by FHI-aims");
+      // note: Jmol DOES support both polymer and slab symmetry.
     }
     super.finalizeReader();
   }
 
-  private void setUnitCellFromLatticeVectors(Vector3f[] abc) {
-    float a = abc[0].length();
-    float b = abc[1].length();
-    float c = abc[2].length();
-    float alpha = (float) Math.toDegrees(abc[1].angle(abc[2]));
-    float beta = (float) Math.toDegrees(abc[2].angle(abc[0]));
-    float gamma = (float) Math.toDegrees(abc[0].angle(abc[1]));
-    // relay length of and angles between cell vectors to Jmol
-    setUnitCell(a, b, c, alpha, beta, gamma);
-    /*
-     * IMPORTANT: also hand over (matrix of) unit cell vectors to trigger the
-     * calculation of the correct transformation matrices from cartesian to
-     * fractional coordinates (which are used internally by Jmol)
-     */
-    float[] lv = new float[3];
-    for (int n = 0; n < 3; n++) {
-      abc[n].get(lv);
-      addPrimitiveLatticeVector(n, lv, 0);
+  /*
+   * lattice_vector 16.503872273 0.000000000 0.000000000
+   */
+  
+  private int nLatticeVectors;
+  
+  private void readLatticeVector(String[] tokens) {
+    if (tokens.length < 4) {
+      Logger.warn("cannot read line with FHI-aims lattice vector: " + line);
+    } else if (nLatticeVectors == 3) {
+      Logger.warn("more than 3 FHI-aims lattice vectors found with line: "
+          + line);
+    } else {
+      addPrimitiveLatticeVector(nLatticeVectors++, new float[] {parseFloat(tokens[1]), parseFloat(tokens[2]), parseFloat(tokens[3])}, 0);
+      setFractionalCoordinates(nLatticeVectors == 3);
     }
   }
+
+  /*
+   * absolute and fractional coordinates are "treated on equal footing" in FHI-aims,
+   * i.e. "atom" and "atom_frac" lines are allowed to appear together in the same geometry.in file
+   * 
+   * atom 2.750645380 2.750645380 25.000000000 Pd
+   * atom_frac 0.25 0.25 0.5 Pd
+   */
+  
+  private void readAtom(String[] tokens, boolean isFractional) {
+    if (tokens.length < 5) {
+      Logger.warn("cannot read line with FHI-aims line: " + line);
+      return;
+    }
+    if (this.isFractional != isFractional)
+      setFractionalCoordinates(this.isFractional = isFractional);
+    Atom atom = atomSetCollection.addNewAtom();
+    setAtomCoord(atom, parseFloat(tokens[1]), parseFloat(tokens[2]),
+        parseFloat(tokens[3]));        
+    atom.elementSymbol = tokens[4];
+  }
+
+  /*
+   * multipole 2.750645380 2.750645380 25.000000000 0 46.0
+   */
+  private void readMultipole(String[] tokens) {
+    if (tokens.length < 6) {
+      Logger.warn("cannot read line with FHI-aims atom data: " + line);
+      return;
+    }
+    int order = parseInt(tokens[4]);
+    if (order > 0) {
+      Logger
+          .warn("multipole line ignored since only monopoles are currently supported: "
+              + line);
+      return;
+    }
+    if (this.isFractional)
+      setFractionalCoordinates(this.isFractional = false);
+    Atom atom = atomSetCollection.addNewAtom();
+    setAtomCoord(atom, parseFloat(tokens[1]), parseFloat(tokens[2]),
+        parseFloat(tokens[3]));
+    atom.partialCharge = parseFloat(tokens[5]);
+    // we generally do not do this: atom.formalCharge = Math.round(atom.partialCharge);
+  }
+
 }
+

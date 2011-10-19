20d391eb44287364c4c7df2f2efe86d0c825c50f
Bob Hanson <hansonr@stolaf.edu>
2011-10-19 11:26:30 +0000

    trying to get threading to work.  Not there yet.
    Former-commit-id: d131bd2531bc7dbf325a90d018a09b3288ae4bb3

--- Jmol-Android/src/org/openscience/jmolandroid/JmolActivity.java
+++ Jmol-Android/src/org/openscience/jmolandroid/JmolActivity.java
@@ -26,54 +26,59 @@ import android.view.ScaleGestureDetector;
 import android.view.SurfaceView;
 
 public class JmolActivity extends Activity implements JmolStatusListener {
-	
 
-  private final static String TEST_SCRIPT = "set debug;set debugscript;load http://chemapps.stolaf.edu/jmol/docs/examples-12/data/caffeine.xyz;labels on; background labels white;spacefill on";
+  private final static String TEST_SCRIPT = "set debug;set debugscript;load http://chemapps.stolaf.edu/jmol/docs/examples-12/data/caffeine.xyz;";
+  //labels on; background labels white;spacefill on";
 
-  private final static String STARTUP_SCRIPT = 
-    "set allowGestures TRUE;unbind ALL _slidezoom;" + TEST_SCRIPT;
+  private final static String STARTUP_SCRIPT = "set allowGestures TRUE;unbind ALL _slidezoom;"
+      + TEST_SCRIPT;
+
+  public static float SCALE_FACTOR;
+
+  private JmolViewer viewer;
+  private SurfaceView imageView;
+  private AndroidUpdateListener updateListener;
+  private boolean opening;
+  private ScaleGestureDetector scaleDetector;
+
+  public SurfaceView getImageView() {
+    return imageView;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    viewer = null;
+    SCALE_FACTOR = getResources().getDisplayMetrics().density + 0.5f;
+
+    scaleDetector = new ScaleGestureDetector(this, new PinchZoomScaleListener());
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    viewer.releaseScreenImage();
+
+    viewer = null;
+    imageView = null;
+    updateListener = null;
+
+    System.exit(0);
+  };
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    updateListener.setPaused(true);    
+  }
   
-	public static float SCALE_FACTOR;
-		
-	private JmolViewer viewer;
-	private SurfaceView imageView;
-	private AndroidUpdateListener updateListener;
-	private boolean opening;
-	private ScaleGestureDetector scaleDetector;
-	
-	
-	public SurfaceView getImageView() {
-	  return imageView;
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	  super.onCreate(savedInstanceState);
-	  setContentView(R.layout.main);
-	  viewer = null;
-	  SCALE_FACTOR = getResources().getDisplayMetrics().density + 0.5f;
-	  
-	  scaleDetector = new ScaleGestureDetector(this, new PinchZoomScaleListener());
-	}
-
-    @Override 
-    protected void onDestroy() {
-    	super.onDestroy();
-        
-    	viewer.releaseScreenImage();
-
-    	viewer = null;
-    	imageView = null;
-    	updateListener = null;
-
-    	System.exit(0);
-    };
-    
   @Override
   protected void onResume() {
     super.onResume();
-    
-    Log.w("AMOL","onResume...");
+
+    Log.w("Jmol", "onResume...");
     // am I correct that imageView is null if and only if viewer is null?
     // otherwise we could have a different imageView in updateListener than here
 
@@ -86,22 +91,17 @@ public class JmolActivity extends Activity implements JmolStatusListener {
       // we pass the updateListener to viewer, where it will be called
       // the "display" and then Platform will get a call asking for an update.
       // not sure about the rest of it!
-      viewer = JmolViewer
-          .allocateViewer(updateListener, new SmarterJmolAdapter(), null, null, null,
-              "platform=org.openscience.jmolandroid.api.Platform", this);
+      viewer = JmolViewer.allocateViewer(updateListener,
+          new SmarterJmolAdapter(), null, null, null,
+          "platform=org.openscience.jmolandroid.api.Platform", this);
       viewer.script(STARTUP_SCRIPT);
     }
 
-    Log.w("AMOL","onResume... viewer=" + viewer);
-    Log.w("AMOL","onResume... opening " + opening);
-    if (viewer.getAtomCount() > 0
-        && !updateListener.isShowingDialog()) {
-      imageView.post(new Runnable() {
-        @Override
-        public void run() {
-          updateListener.updateCanvas();
-        }
-      });
+    Log.w("Jmol", "onResume... viewer=" + viewer);
+    Log.w("Jmol", "onResume... opening " + opening);
+    updateListener.setPaused(false);    
+    if (viewer.getAtomCount() > 0 && !updateListener.isShowingDialog()) {
+      updateListener.repaintEvent();
     } else {
       //        	this.setTitle(R.string.app_name);
       if (!opening) {
@@ -116,211 +116,225 @@ public class JmolActivity extends Activity implements JmolStatusListener {
     }
 
   };
-    
-    @Override
-    public void onBackPressed() {
-    	this.finish();
-    };
-    
-    @Override
-    public boolean onTouchEvent(final MotionEvent event) {
-    	// don't process other touch events when zooming
-/*    	if (scaleDetector.onTouchEvent(event))
-    		return true;
-*/
-    	scaleDetector.onTouchEvent(event);
-    	
-    	
-//      Log.w("AMOL","onTouchEvent " + event);
-    	switch (event.getAction()) {
-    		case MotionEvent.ACTION_DOWN:
-    			new AsyncTask<MotionEvent, Void, Void>(){
-					@Override
-					protected Void doInBackground(MotionEvent... event) {
-			    		viewer.mouseEvent(Event.MOUSE_DOWN, (int)event[0].getX(), (int)event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
-			    		return null;
-					}}.execute(event);
-    	    	break;
-    		case MotionEvent.ACTION_UP:
-    			new AsyncTask<MotionEvent, Object, Object>(){
-					@Override
-					protected Void doInBackground(MotionEvent... event) {
-			    		viewer.mouseEvent(Event.MOUSE_UP, (int)event[0].getX(), (int)event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
-			    		return null;
-					}}.execute(event);
-	    		break;
-    		case MotionEvent.ACTION_MOVE:
-    			new AsyncTask<MotionEvent, Object, Object>(){
-					@Override
-					protected Void doInBackground(MotionEvent... event) {
-			    		viewer.mouseEvent(Event.MOUSE_DRAG, (int)event[0].getX(), (int)event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
-			    		return null;
-					}}.execute(event);
-	    		break;
-    	}
-    	
-		return true;
-    };
-    
-    private class PinchZoomScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-    	@Override
-    	public boolean onScale(final ScaleGestureDetector detector) {
-    		Log.w("JMOL", "old zoom=" + viewer.getZoomPercentFloat());
-    		
-	        float zoomFactor = detector.getScaleFactor() / (viewer.getZoomPercentFloat() / 100.0f);
-	        viewer.syncScript("Mouse: zoomByFactor " + zoomFactor, "~", 0);
-    		Log.w("JMOL", "changing zoom factor=" + zoomFactor);
 
-    		return true;
-    	}
-    }
-    
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-		Log.w("AMOL","onWindowFocusChanged " + hasFocus);
-		if (!hasFocus) return;
-		updateListener.setScreenDimension();
-    };
-    
-    
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-    	MenuItem styleMenu = menu.findItem(R.id.style);
-    	
-    	if (styleMenu != null)
-    		styleMenu.setEnabled(viewer.getAtomCount() > 0);
-    	
-    	return true;
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-    
-    private static final int REQUEST_OPEN = 1;
+  // TODO Auto-generated method stub
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-        case R.id.open:
-    		File path = Downloader.getAppDir(this);
-        	
-        	Intent intent = new Intent(this.getBaseContext(), FileDialog.class);
-			intent.putExtra(FileDialog.START_PATH, path.getAbsolutePath());
-			this.startActivityForResult(intent, REQUEST_OPEN);
-            break;
-        case R.id.download:
-        	Intent dnIntent = new Intent(this.getBaseContext(), PDBSearchActivity.class);
-			this.startActivityForResult(dnIntent, REQUEST_OPEN);
-        	break;
-		case R.id.cpkspacefill:
-			viewer.evalString("restrict bonds not selected;select not selected;spacefill 100%;color cpk");
-			break;
-		case R.id.ballandstick:
-			viewer.evalString("restrict bonds not selected;select not selected;spacefill 23%AUTO;wireframe 0.15;color cpk");
-			break;
-		case R.id.sticks:
-			viewer.evalString("restrict bonds not selected;select not selected;wireframe 0.3;color cpk");
-			break;
-		case R.id.wireframe:
-			viewer.evalString("restrict bonds not selected;select not selected;wireframe on;color cpk");
-			break;
-		case R.id.cartoon:
-			viewer.evalString("restrict bonds not selected;select not selected;cartoons on;color structure");
-			break;
-		case R.id.trace:
-			viewer.evalString("restrict bonds not selected;select not selected;trace on;color structure");
-			break;
-        default:
-            return super.onOptionsItemSelected(item);
+  @Override
+  public void onBackPressed() {
+    this.finish();
+  };
+
+  @Override
+  public boolean onTouchEvent(final MotionEvent event) {
+    // don't process other touch events when zooming
+    /*    	if (scaleDetector.onTouchEvent(event))
+        		return true;
+    */
+    scaleDetector.onTouchEvent(event);
+
+    //      Log.w("Jmol","onTouchEvent " + event);
+    switch (event.getAction()) {
+    case MotionEvent.ACTION_DOWN:
+      new AsyncTask<MotionEvent, Void, Void>() {
+        @Override
+        protected Void doInBackground(MotionEvent... event) {
+          viewer.mouseEvent(Event.MOUSE_DOWN, (int) event[0].getX(),
+              (int) event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
+          return null;
         }
-        
-        return true;
+      }.execute(event);
+      break;
+    case MotionEvent.ACTION_UP:
+      new AsyncTask<MotionEvent, Object, Object>() {
+        @Override
+        protected Void doInBackground(MotionEvent... event) {
+          viewer.mouseEvent(Event.MOUSE_UP, (int) event[0].getX(),
+              (int) event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
+          return null;
+        }
+      }.execute(event);
+      break;
+    case MotionEvent.ACTION_MOVE:
+      new AsyncTask<MotionEvent, Object, Object>() {
+        @Override
+        protected Void doInBackground(MotionEvent... event) {
+          viewer.mouseEvent(Event.MOUSE_DRAG, (int) event[0].getX(),
+              (int) event[0].getY(), Event.MOUSE_LEFT, event[0].getDownTime());
+          return null;
+        }
+      }.execute(event);
+      break;
     }
-    
-    public synchronized void onActivityResult(final int requestCode, int resultCode, final Intent data) {
-            if (resultCode == Activity.RESULT_OK) {
 
-                    if (requestCode == REQUEST_OPEN) {
-                    	updateListener.manageDialog(ProgressDialog.show(this, "", "Opening file...", true), (byte)2);
-                        viewer.openFileAsynchronously(data.getStringExtra(FileDialog.RESULT_PATH));
-                        opening = true;
-                    }
-            }
-    }
+    return true;
+  };
 
+  private class PinchZoomScaleListener extends
+      ScaleGestureDetector.SimpleOnScaleGestureListener {
     @Override
-    public String createImage(String fileName, String type, Object textOrBytes,
-                              int quality) {
-      // ignore
-      // TODO Auto-generated method stub
-      return null;
+    public boolean onScale(final ScaleGestureDetector detector) {
+      Log.w("JMOL", "old zoom=" + viewer.getZoomPercentFloat());
+
+      float zoomFactor = detector.getScaleFactor()
+          / (viewer.getZoomPercentFloat() / 100.0f);
+      viewer.syncScript("Mouse: zoomByFactor " + zoomFactor, "~", 0);
+      Log.w("JMOL", "changing zoom factor=" + zoomFactor);
+
+      return true;
+    }
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    Log.w("Jmol", "onWindowFocusChanged " + hasFocus);
+    if (!hasFocus)
+      return;
+    updateListener.setScreenDimension();
+  };
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    MenuItem styleMenu = menu.findItem(R.id.style);
+
+    if (styleMenu != null)
+      styleMenu.setEnabled(viewer.getAtomCount() > 0);
+
+    return true;
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  private static final int REQUEST_OPEN = 1;
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+    case R.id.open:
+      File path = Downloader.getAppDir(this);
+
+      Intent intent = new Intent(this.getBaseContext(), FileDialog.class);
+      intent.putExtra(FileDialog.START_PATH, path.getAbsolutePath());
+      this.startActivityForResult(intent, REQUEST_OPEN);
+      break;
+    case R.id.download:
+      Intent dnIntent = new Intent(this.getBaseContext(),
+          PDBSearchActivity.class);
+      this.startActivityForResult(dnIntent, REQUEST_OPEN);
+      break;
+    case R.id.cpkspacefill:
+      viewer.script("spacefill only;color cpk");
+      break;
+    case R.id.ballandstick:
+      viewer.script("wireframe -0.15;spacefill 23%;color cpk");
+      break;
+    case R.id.sticks:
+      viewer.script("wireframe -0.3;color cpk");
+      break;
+    case R.id.wireframe:
+      viewer.script("wireframe only;color cpk");
+      break;
+    case R.id.cartoon:
+      viewer.script("cartoons only;color cartoons structure");
+      break;
+    case R.id.trace:
+      viewer.script("trace only;color trace structure");
+      break;
+    default:
+      return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public String eval(String strEval) {
-      // ignore
-      // TODO Auto-generated method stub
-      return null;
-    }
+    return true;
+  }
 
-    @Override
-    public float[][] functionXY(String functionName, int x, int y) {
-      // ignore
-      // TODO Auto-generated method stub
-      return null;
-    }
+  public synchronized void onActivityResult(final int requestCode,
+                                            int resultCode, final Intent data) {
+    if (resultCode == Activity.RESULT_OK) {
 
-    @Override
-    public float[][][] functionXYZ(String functionName, int nx, int ny, int nz) {
-      // ignore
-      // TODO Auto-generated method stub
-      return null;
+      if (requestCode == REQUEST_OPEN) {
+        updateListener.manageDialog(ProgressDialog.show(this, "",
+            "Opening file...", true), (byte) 2);
+        // this is the best way to go -- allows for scripts, surfaces, and models
+        viewer.openFileAsynchronously(data
+            .getStringExtra(FileDialog.RESULT_PATH));
+        opening = true;
+      }
     }
+  }
 
-    @Override
-    public Map<String, Object> getRegistryInfo() {
-      // ignore
-      // TODO Auto-generated method stub
-      return null;
-    }
+  @Override
+  public String createImage(String fileName, String type, Object textOrBytes,
+                            int quality) {
+    // ignore
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public void resizeInnerPanel(String data) {
-      // ignore
-      // TODO Auto-generated method stub
-      
-    }
+  @Override
+  public String eval(String strEval) {
+    // ignore
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public void showUrl(String url) {
-      // ignore
-      // TODO Auto-generated method stub      
-    }
+  @Override
+  public float[][] functionXY(String functionName, int x, int y) {
+    // ignore
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public void notifyCallback(EnumCallback message, Object[] data) {
-      // probably ignore
-      // TODO Auto-generated method stub
-      
-    }
+  @Override
+  public float[][][] functionXYZ(String functionName, int nx, int ny, int nz) {
+    // ignore
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public boolean notifyEnabled(EnumCallback type) {
-      // probably ignore
-      // TODO Auto-generated method stub
-      return false;
-    }
+  @Override
+  public Map<String, Object> getRegistryInfo() {
+    // ignore
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-    @Override
-    public void setCallbackFunction(String callbackType, String callbackFunction) {
-      // ignore -- applet only
-      // TODO Auto-generated method stub
-      
-    }
+  @Override
+  public void resizeInnerPanel(String data) {
+    // ignore
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void showUrl(String url) {
+    // ignore
+    // TODO Auto-generated method stub      
+  }
+
+  @Override
+  public void notifyCallback(EnumCallback message, Object[] data) {
+    // probably ignore
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean notifyEnabled(EnumCallback type) {
+    // probably ignore
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void setCallbackFunction(String callbackType, String callbackFunction) {
+    // ignore -- applet only
+    // TODO Auto-generated method stub
+
+  }
 
 }
--- Jmol-Android/src/org/openscience/jmolandroid/api/AndroidUpdateListener.java
+++ Jmol-Android/src/org/openscience/jmolandroid/api/AndroidUpdateListener.java
@@ -10,19 +10,19 @@ import android.view.SurfaceView;
 
 public class AndroidUpdateListener {
 
-	private JmolViewer viewer;
-	private byte counter;
-	private Dialog dialog;
-	private JmolActivity ja;
-	
-	public AndroidUpdateListener(JmolActivity ja) {
-	  this.ja = ja;
-	}
-		
-	void setViewer(JmolViewer viewer) {
-	  this.viewer = viewer;
-	}
-	
+  private JmolViewer viewer;
+  private byte counter;
+  private Dialog dialog;
+  private JmolActivity ja;
+
+  public AndroidUpdateListener(JmolActivity ja) {
+    this.ja = ja;
+  }
+
+  void setViewer(JmolViewer viewer) {
+    this.viewer = viewer;
+  }
+
   public void setScreenDimension() {
     int width = ja.getImageView().getWidth();
     int height = ja.getImageView().getHeight();
@@ -30,45 +30,69 @@ public class AndroidUpdateListener {
       viewer.setScreenDimension(width, height);
   }
 
-	public void repaint() {
-		updateCanvas();
-		
-    	if (dialog != null && --counter <= 0) {
-    		dialog.dismiss();
-    		dialog = null;
-    	}
-	}
+  public void repaint() {
+    // from Viewer
+    if (dialog != null && --counter <= 0) {
+      dialog.dismiss();
+      dialog = null;
+    }
+    repaintEvent();
+  }
 
-	public void updateCanvas() {
-		SurfaceView imageView = ja.getImageView();
-		long start = System.currentTimeMillis();
-		
-		synchronized (imageView) {
-		    Canvas canvas = null;
-			try {
-				canvas = imageView.getHolder().lockCanvas();
-				canvas.getHeight(); // simple test for canvas not null
-			} catch(Exception e) {
-				Log.w("AMOL", "Unable to lock the canvas\n");             
-			  //e.printStackTrace();
-			}
-			if (canvas != null) {
-				// at least for now we want to see errors traced to their Jmol methods, not trapped here
-				viewer.renderScreenImage(canvas, null, canvas.getWidth(), canvas.getHeight());
-				imageView.getHolder().unlockCanvasAndPost(canvas);
-			}
-		}
-		Log.d("AMOL", "Image updated in " + (System.currentTimeMillis() - start) + " ms");
-		Log.d("AMOL", "Zoom % " + viewer.getZoomPercentFloat());
-	}
-	
-	public void manageDialog(Dialog dialog, byte value) {
-		this.dialog = dialog;
-		counter = value;
-	}
-	
-	public boolean isShowingDialog() {
-		return dialog != null;
-	}
+  public void repaintEvent() {
+    // from onResume
+    ja.getImageView().post(new Runnable() {
+      @Override
+      public void run() {
+        updateCanvas();
+      }
+    });
+  }
+
+  private boolean paused;
+  private boolean updating;
+  
+  protected void updateCanvas() {
+    Log.w("Jmol","updateCanvas paused/updating " + paused + " " + updating);
+    if (paused || updating)
+      return;
+    SurfaceView imageView = ja.getImageView();
+    long start = System.currentTimeMillis();
+
+    synchronized (imageView) {
+      updating = true;
+      Canvas canvas = null;
+      try {
+        canvas = imageView.getHolder().lockCanvas();
+        canvas.getHeight(); // simple test for canvas not null
+      } catch (Exception e) {
+        Log.w("Jmol", "Unable to lock the canvas\n");
+        //e.printStackTrace();
+      }
+      if (canvas != null) {
+        // at least for now we want to see errors traced to their Jmol methods, not trapped here
+        viewer.renderScreenImage(canvas, null, canvas.getWidth(), canvas
+            .getHeight());
+        imageView.getHolder().unlockCanvasAndPost(canvas);
+      }
+      updating = false;
+    }
+    Log.d("Jmol", "Image updated in " + (System.currentTimeMillis() - start)
+        + " ms");
+    Log.d("Jmol", "Zoom % " + viewer.getZoomPercentFloat());
+  }
+
+  public void setPaused(boolean TF) {
+    paused = TF;    
+  }
+
+  public void manageDialog(Dialog dialog, byte value) {
+    this.dialog = dialog;
+    counter = value;
+  }
+
+  public boolean isShowingDialog() {
+    return dialog != null;
+  }
 
 }

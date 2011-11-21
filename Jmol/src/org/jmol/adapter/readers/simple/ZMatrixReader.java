010fdc37ea0b070ad4d058b3e993cd0b79241ac9
Bob Hanson <hansonr@stolaf.edu>
2011-11-21 02:37:14 +0000

    Former-commit-id: 463d2dabbdd42e9399a0dd4ec45f44cd816b3dec

--- Jmol/src/org/jmol/adapter/readers/simple/ZMatrixReader.java
+++ Jmol/src/org/jmol/adapter/readers/simple/ZMatrixReader.java
@@ -336,6 +336,7 @@ d300     300.0
       v2 = (new Quaternion(v2, theta1)).transform(v1);
       v2 = (new Quaternion(v1, -theta2)).transform(v2);
     } else {
+      // d < 0
       // theta1 and theta2 are simple angles atom-ia-ib and atom-ia-ic 
       // get vector that is intersection of two planes and go from there
       Measure.getPlaneThroughPoint(setAtom(atom, ia, ib, ic, -d, theta1, 0), v1, plane1);

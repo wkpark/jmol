47bd9e4ab2c60d88c35a4c78f5d2a8a1c3cbec28
Bob Hanson <hansonr@stolaf.edu>
2012-03-08 22:07:26 +0000

    Former-commit-id: f8e02150697a25939b8a82b09098889a612a2f2e

--- Jmol/src/org/jmol/api/JSVInterface.java
+++ Jmol/src/org/jmol/api/JSVInterface.java
@@ -8,9 +8,10 @@ import java.util.Properties;
  */
 public interface JSVInterface {
   
-  public void runScript(String script);
-  public void setProperties(Properties properties);  
-  public void saveProperties(Properties properties);
   public void exitJSpecView(boolean withDialog, Object frame);
+  public void runScript(String script);
+  public void saveProperties(Properties properties);
+  public void setProperties(Properties properties);  
+  public void syncToJmol(String msg);
 
 }

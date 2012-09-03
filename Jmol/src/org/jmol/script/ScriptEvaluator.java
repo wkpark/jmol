1fdea5948109e5aecf30a36fcf9527ce83d368a3
Bob Hanson <hansonr@stolaf.edu>
2012-09-03 13:44:58 +0000

    ___version=13.1.2
    new feature: cache ADD "filename" 
      -- adds a file into the memory cache as a set of bytes
    new feature: cache REMOVE "filename"|ALL
      -- removes a file from the cache
    new feature: show cache
      -- displays the cache as an array
    new feature: cache() function
      -- returns the cache as an associative array {filename: nBytes, filename: nbytes,...}
    new feature: write INLINE "xxxx" "filename"
        for example: write INLINE "testing" "myfile.txt"
        for example: write INLINE @{load("$caffeine")} "caffeine.mol"
    new feature: set defaultDropScript for drag-drop and File|Open
    new feature: (JmolViewer) public void cacheFile(String fileName, byte[] bytes)
      -- allows a mechanism for applets or embedding apps to deliver file content as bytes
      -- for applets, first getPropertyAsJavaObject("Viewer")
    
    bug fix: translate x 0 does not recenter the model in the window
    bug fix: PDB polymers of length 1 allow size to be set and appear {visible} but are not
    bug fix: OPEN dialog doesn't allow for no PDB cartoons.
    bug fix: open dialog for 1-residue PDB files does not show atoms.
    bug fix: pngj storage of Spartan directory zip files or Spartan directories cannot be read
             because the PNGJ files created do not contain the necessary files from the directory
     
    code: refactored for Java2Script due to inner class bug there.
    
    
    
    Former-commit-id: d55ebd1bac439fb1b08a4debc8c69b8bd5142826

--- Jmol/src/org/jmol/script/ScriptEvaluator.java
+++ Jmol/src/org/jmol/script/ScriptEvaluator.java
@@ -10909,8 +10909,6 @@ public class ScriptEvaluator {
     if (xyz != Token.x && xyz != Token.y && xyz != Token.z)
       error(ERROR_axisExpected);
     float amount = floatParameter(++i);
-    if (amount == 0)
-      return;
     char type;
     switch (tokAt(++i)) {
     case Token.nada:
@@ -10921,6 +10919,8 @@ public class ScriptEvaluator {
     default:
       type = (optParameterAsString(i).toLowerCase() + '\0').charAt(0);
     }
+    if (amount == 0 && type != '\0')
+      return;
     iToken = (type == '\0' ? 2 : 3);
     bs = (isSelected ? viewer.getSelectionSet(false)
         : iToken + 1 < statementLength ? atomExpression(++iToken) : null);

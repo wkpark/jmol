bcbb2203873ae5fccba2507a1179eaa5d9ee4faf
Bob Hanson <hansonr@stolaf.edu>
2012-10-02 23:25:00 +0000

    explicit mention of outputFooter
    Former-commit-id: 41b88b61a09cfa49e8384b07c0700f26481660c5

--- Jmol/src/org/jmol/exportjs/_JSExporter.java
+++ Jmol/src/org/jmol/exportjs/_JSExporter.java
@@ -34,38 +34,41 @@ import javax.vecmath.Tuple3f;
 
 import org.jmol.util.J2SRequireImport;
 
-@J2SRequireImport({org.jmol.exportjs.___Exporter.class, org.jmol.exportjs.__CartesianExporter.class, org.jmol.exportjs.Export3D.class})
+@J2SRequireImport( { org.jmol.exportjs.___Exporter.class,
+    org.jmol.exportjs.__CartesianExporter.class,
+    org.jmol.exportjs.Export3D.class })
 public class _JSExporter extends __CartesianExporter {
 
   private UseTable useTable;
 
   public _JSExporter() {
-  useTable = new UseTable();
+    useTable = new UseTable();
   }
 
-	@Override
-	protected void outputFace(int[] is, int[] coordMap, int faceVertexMax) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  protected void outputFace(int[] is, int[] coordMap, int faceVertexMax) {
+    // TODO Auto-generated method stub
+
+  }
 
   private Map<String, Boolean> htSpheresRendered = new Hashtable<String, Boolean>();
 
   private Map<String, Object[]> htObjects = new Hashtable<String, Object[]>();
-  
+
   private void useSphere(String id, boolean isNew, Point3f pt, Object[] o) {
     // implemented in JavaScript only
     System.out.println(id + " " + isNew + " " + pt + " " + o);
   }
-  
-  private void useCylinder(String id, boolean isNew, Point3f pt1, Point3f pt2, Object[] o) {
+
+  private void useCylinder(String id, boolean isNew, Point3f pt1, Point3f pt2,
+                           Object[] o) {
     // implemented in JavaScript only
     System.out.println(id + " " + isNew + " " + pt1 + " " + pt2 + " " + o);
   }
 
-	@Override
-	protected void outputSphere(Point3f ptCenter, float radius, short colix,
-			boolean checkRadius) {
+  @Override
+  protected void outputSphere(Point3f ptCenter, float radius, short colix,
+                              boolean checkRadius) {
     int iRad = (int) (radius * 100);
     String check = round(ptCenter) + (checkRadius ? " " + iRad : "");
     if (htSpheresRendered.get(check) != null)
@@ -76,50 +79,52 @@ public class _JSExporter extends __CartesianExporter {
     if (found)
       o = htObjects.get(ret[0]);
     else
-      htObjects.put(ret[0], o = new Object[] {getColor(colix), Float.valueOf(radius)});
+      htObjects.put(ret[0], o = new Object[] { getColor(colix),
+          Float.valueOf(radius) });
     useSphere(ret[0], !found, ptCenter, o);
-	}
+  }
 
-	private String[] ret = new String[1];
-	
+  private String[] ret = new String[1];
+
   @Override
   protected boolean outputCylinder(Point3f ptCenter, Point3f pt1, Point3f pt2,
-      short colix, byte endcaps, float radius, Point3f ptX, Point3f ptY,
-      boolean checkRadius) {
+                                   short colix, byte endcaps, float radius,
+                                   Point3f ptX, Point3f ptY, boolean checkRadius) {
     // ptX and ptY are ellipse major and minor axes
     // not implemented yet
     if (ptX != null)
       return false;
     float length = pt1.distance(pt2);
-    boolean found = useTable.getDef("C" + colix + "_" + (int) (length * 100) + "_" + radius
-        + "_" + endcaps, ret);
+    boolean found = useTable.getDef("C" + colix + "_" + (int) (length * 100)
+        + "_" + radius + "_" + endcaps, ret);
     Object[] o;
     if (found)
       o = htObjects.get(ret[0]);
     else
-      htObjects.put(ret[0], o = new Object[] { getColor(colix), new Float(length), new Float(radius) });
+      htObjects.put(ret[0], o = new Object[] { getColor(colix),
+          new Float(length), new Float(radius) });
     useCylinder(ret[0], !found, pt1, pt2, o);
     return true;
   }
 
   @Override
   protected void outputCircle(Point3f pt1, Point3f pt2, float radius,
-      short colix, boolean doFill) {
+                              short colix, boolean doFill) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   protected void outputCone(Point3f ptBase, Point3f ptTip, float radius,
-      short colix) {
+                            short colix) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   protected void outputEllipsoid(Point3f center, Point3f[] points, short colix) {
     // TODO Auto-generated method stub
-    
+
   }
 
   private Integer getColor(short colix) {
@@ -129,40 +134,46 @@ public class _JSExporter extends __CartesianExporter {
   void addObject(String id, Object o) {
     // id will be "_S1" or "_C1"
     // implemented in JavaScript only
-     /**
-      * @j2sNative
-      * 
-      */
-     {
-       System.out.println(id + " " + o);
-     }
+    /**
+     * @j2sNative
+     * 
+     */
+    {
+      System.out.println(id + " " + o);
+    }
   }
 
-	@Override
-	protected void outputTextPixel(Point3f pt, int argb) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  protected void outputTextPixel(Point3f pt, int argb) {
+    // TODO Auto-generated method stub
 
-	@Override
-	protected void outputTriangle(Point3f pt1, Point3f pt2, Point3f pt3,
-			short colix) {
-		// TODO Auto-generated method stub
-		
-	}
+  }
 
-	@Override
-	protected void output(Tuple3f pt) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  protected void outputTriangle(Point3f pt1, Point3f pt2, Point3f pt3,
+                                short colix) {
+    // TODO Auto-generated method stub
 
-	@Override
-	protected void outputHeader() {
-		// TODO Auto-generated method stub
-		
-	}
-  
+  }
+
+  @Override
+  protected void output(Tuple3f pt) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  protected void outputHeader() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  protected void outputFooter() {
+    // TODO Auto-generated method stub
+
+  }
+
 }
 
 

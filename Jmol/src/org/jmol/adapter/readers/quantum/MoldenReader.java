305200ef15704745b6c5e8d88af2cbd3299b0166
Bob Hanson <hansonr@stolaf.edu>
2012-11-23 14:13:33 +0000

    Former-commit-id: 8669f26b013f98d271bb2f0295dbbd52d84a6436

--- Jmol/src/org/jmol/adapter/readers/quantum/MoldenReader.java
+++ Jmol/src/org/jmol/adapter/readers/quantum/MoldenReader.java
@@ -120,8 +120,7 @@ public class MoldenReader extends MopacSlaterReader {
      H     5    1        -0.4338802400       -0.3282176500       -0.9384614500
      H     6    1        -0.4338802400       -0.3282176500        0.9384614500
      */
-    String s = line.replace(']', ' ');
-    String coordUnit = getTokensStr(s)[1];
+    String coordUnit = getTokensStr(line.replace(']', ' '))[1];
     
     int nPrevAtom = 0, nCurAtom = 0;
    

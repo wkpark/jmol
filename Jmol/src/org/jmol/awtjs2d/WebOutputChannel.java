fa8c310470eb18534cee2ac4c04e2bd30f5c5fd2
Bob Hanson <hansonr@stolaf.edu>
2013-09-24 19:32:44 +0000

    Former-commit-id: a7c5b46b291353454269721a24ad439dc0784a0e

--- Jmol/src/org/jmol/awtjs2d/WebOutputChannel.java
+++ Jmol/src/org/jmol/awtjs2d/WebOutputChannel.java
@@ -1,55 +1,8 @@
 package org.jmol.awtjs2d;
 
-import java.io.ByteArrayOutputStream;
+import org.jmol.io.JmolOutputChannel;
 
-import org.jmol.api.JmolOutputChannel;
-import org.jmol.util.SB;
 
-/**
- * 
- * A surrogate for FileOutputStream, allowing collection of byte
- * or character data for final posting to a server as bytes to
- * be returned to the user via a browser dialog. HTML5 JS only.
- *  
- */
-
-public class WebOutputChannel extends ByteArrayOutputStream  implements JmolOutputChannel {
+public class WebOutputChannel extends JmolOutputChannel {
  
-  private String fileName;
-  
-  public String getFileName() {
-    return fileName;
-  }
-  
-  private SB sb;
-  
-  public WebOutputChannel(String fileName) {
-    this.fileName = fileName;
-  }
-
-  public void cancel() {
-    fileName = null;
-    sb = null;
-  }
-  
-  public void write(String data) {
-    if (sb == null)
-      sb = new SB();
-    sb.append(data);
-  }
-
-  @Override
-  public void close() {
-    if (fileName == null)
-      return;
-
-    /**
-     * @j2sNative
-     * 
-     *     Jmol._doAjax(this.fileName, null, (this.sb == null ? this.toByteArray() : this.sb.toString()));
-     * 
-     */
-    {
-    }
-  }
 }

235ac5d7c71fd8d46084544831af16abcbcb0ed8
Bob Hanson <hansonr@stolaf.edu>
2013-09-24 19:32:17 +0000

    Former-commit-id: a800747ee311856a3c3c5969d3c0d52801ca0cc4

--- Jmol/src/org/jmol/awt/LocalOutputChannel.java
+++ Jmol/src/org/jmol/awt/LocalOutputChannel.java
@@ -1,41 +1,7 @@
 package org.jmol.awt;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.jmol.io.JmolOutputChannel;
 
-import org.jmol.api.JmolOutputChannel;
+public class LocalOutputChannel extends JmolOutputChannel {
 
-/**
- * 
- * A wrapper for FileOutputStream, allowing debugging.
- * 
- *  
- */
-
-public class LocalOutputChannel extends FileOutputStream implements JmolOutputChannel {
- 
-  private String fileName;
-  
-  public String getFileName() {
-    return fileName;
-  }
-  
-  public LocalOutputChannel(String fileName) throws FileNotFoundException {
-    super(fileName);    
-    this.fileName = fileName;
-  }
-
-  public void cancel() {
-    close();
-  }
-  
-  @Override
-  public void close() {
-    try {
-      super.close();
-    } catch (IOException e) {
-      // TODO
-    }
-  }
 }

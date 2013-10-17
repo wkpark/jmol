f7d1ed7350e0397ca43794b1b51c12f987a380d5
Bob Hanson <hansonr@stolaf.edu>
2013-10-17 17:28:19 +0000

    Former-commit-id: 2384dd5dc1730f258c53d51765b6c471fb8f8c44

--- Jmol/src/javajs/util/T3.java
+++ Jmol/src/javajs/util/T3.java
@@ -14,7 +14,7 @@
    makes no representations about the suitability of this software for any
    purpose.  It is provided "AS IS" with NO WARRANTY.
 */
-package javajs.vec;
+package javajs.util;
 
 import java.io.Serializable;
 

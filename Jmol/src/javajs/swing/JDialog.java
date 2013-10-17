c6195308c6d0400f06f767e495b23752da5d36a8
Bob Hanson <hansonr@stolaf.edu>
2013-10-17 20:06:41 +0000

    Former-commit-id: 6604d01cf6f62da41bf2115999f2710015657fb0

--- Jmol/src/javajs/swing/JDialog.java
+++ Jmol/src/javajs/swing/JDialog.java
@@ -94,8 +94,8 @@ public class JDialog extends JContainer {
 		SB sb = new SB();
 		sb.append("\n<div id='" + id + "' class='JDialog' style='" + getCSSstyle(0) + "position:relative;top:0px;left:0px;reize:both;'>\n");
 	  sb.append("\n<div id='" + id + "_title' class='JDialogTitle' style='width:100%;height:25px;padding:5px 5px 5px 5px;height:"+headerHeight+"px'>"
-	  		+"<span style='text-align:center;'>" + title + "</span><span style='position:absolute;text-align:right;right:1px;>"
-	  		+ "<a id='" + id + "_closer' href='javascript:Jmol.Dialog.windowClosing(this)'>[x]</a></span></div>\n");
+	  		+"<span style='text-align:center;'>" + title + "</span><span style='position:absolute;text-align:right;right:1px;'>"
+	  		+ "<input type=button id='" + id + "_closer' href='javascript:Jmol.Dialog.windowClosing(this)' value='x' /></span></div>\n");
 	  sb.append("\n<div id='" + id + "_body' class='JDialogBody' style='width:100%;height:"+h+"px;"
 	  		+"position:	relative;left:0px;top:0px'>\n");
 	  sb.append(contentPane.toHTML());

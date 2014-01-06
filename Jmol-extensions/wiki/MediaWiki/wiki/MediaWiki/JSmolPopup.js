cd7b4680fa6f2485ee81c03c93fd3e7dc7aa0c41
Jaime Prilusky <lsprilus@users.sourceforge.net>
2014-01-06 10:28:33 +0000

    Misplaced directory
    Former-commit-id: 8948821830ff71bed9588008f45f30b4ae4fc131

--- Jmol-extensions/wiki/MediaWiki/wiki/MediaWiki/JSmolPopup.js
+++
@@ -1,8 +0,0 @@
-var JSmolCloneData = {};
-function cloneJSmol(JSmolObject) {
-  var t = JSmolObject._jmolType; 
-  JSmolCloneData.type = t;
-  JSmolCloneData.state = Jmol.getPropertyAsString(JSmolObject, 'stateInfo');
-  window.open('/wiki/extensions/jsmol/wiki/JSmolPopup.htm','JSmolPopup','resizable, width=500, height=500');
-}
-

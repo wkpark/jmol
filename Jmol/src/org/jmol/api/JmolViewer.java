def2eae8e121b90eb5e15f7820de776d96be3dca
Bob Hanson <hansonr@stolaf.edu>
2014-02-01 16:33:57 +0000

    Former-commit-id: d8b8f958eedb62018192af41c10824ec641035a6

--- Jmol/src/org/jmol/api/JmolViewer.java
+++ Jmol/src/org/jmol/api/JmolViewer.java
@@ -373,7 +373,7 @@ abstract public class JmolViewer implements JSInterface {
   abstract public void addSelectionListener(JmolSelectionListener listener);
   abstract public void removeSelectionListener(JmolSelectionListener listener);
   
-  abstract public BS getSelectionSet();
+  abstract public BS getSelectionSet(boolean includeDeleted);
 
   abstract public void homePosition();
 

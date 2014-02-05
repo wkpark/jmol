06cf5dde9e2ddd25c75fe66067fb5ae54bd0ede2
Bob Hanson <hansonr@stolaf.edu>
2014-02-05 07:19:40 +0000

    Former-commit-id: 8b9fae6305e8bf409baf0e68a6963624917bd83c

---
+++ Jmol/src/javajs/swing/JMenuItem.java
@@ -0,0 +1,36 @@
+package javajs.swing;
+
+import javajs.util.PT;
+
+public class JMenuItem extends AbstractButton {
+
+  private int btnType;
+
+  public JMenuItem(String text) {
+    super("btn");
+    setText(text);
+  }
+
+  public JMenuItem(String type, int i) {
+    super(type);
+    btnType = i;
+  }
+
+  @Override
+  public String toHTML() {
+      String s = "<li id=\"~ID~\" class=\"" + (this.enabled ? "" : "ui-state-disabled") + "\">";
+      if (this.text != null)
+        s += "<a>";
+      s += htmlLabel();
+      s = PT.rep(s, "~ID~", this.id);    
+      if (this.text != null) 
+        s = PT.rep(s,"TeXt", this.text) + "</a>";
+      s += "</li>";
+      return s;
+  }
+
+  protected String htmlLabel() {
+    return (this.text != null ? "TeXt" : "");
+  }
+  
+}

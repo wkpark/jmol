fe52359b11d2a0f095af910db6adec70b5eaaf84
Bob Hanson <hansonr@stolaf.edu>
2014-02-10 00:31:31 +0000

    Former-commit-id: 01a1e29b98838180d332c9019ad8049fed6accae

---
+++ Jmol/src/unused/Export3D.java
@@ -0,0 +1,40 @@
+/* $RCSfile$
+ * $Author: hansonr $
+ * $Date: 2007-10-07 20:10:15 -0500 (Sun, 07 Oct 2007) $
+ * $Revision: 8384 $
+ *
+ * Copyright (C) 2003-2006  Miguel, Jmol Development, www.jmol.org
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package unused;
+
+
+/**
+ * An interface to JmolJSO.js
+ * 
+ * @author hansonr, hansonr@stolaf.edu
+ * 
+ */
+
+final public class Export3D extends GenericExport3D {
+
+  public Export3D() {
+  }
+
+
+}

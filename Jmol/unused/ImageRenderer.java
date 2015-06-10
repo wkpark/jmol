6b6c33b85c672292aa36317e6f76bc0ef80a4b4b
Bob Hanson <hansonr@stolaf.edu>
2015-06-10 11:54:54 +0000

    Jmol.___JmolVersion="14.3.14_2015.06.10c"
    bug fix: "transparent" PNG images as echos in front still hide pixels
    
    code : PNG images with partial transparency are not supported. 
           They COULD be supported, but currently the rendering is
           in the first pass, but it could be in a later pass.    
    
    code: g3d.ImageRenderer removed; code moved to g3d
    
    
    
    
    Former-commit-id: ace923708b057600d5a77b71570e3e54b04337f0

---
+++ Jmol/unused/ImageRenderer.java
@@ -0,0 +1,50 @@
+/* $RCSfile$
+ * $Author: hansonr $
+ * $Date: 2011-10-10 20:09:00 -0500 (Mon, 10 Oct 2011) $
+ * $Revision: 16309 $
+ *
+ * Copyright (C) 2003-2005  Miguel, Jmol Development, www.jmol.org
+ *
+ * Contact: jmol-developers@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jmol.g3d;
+
+import org.jmol.api.JmolRendererInterface;
+
+class ImageRenderer {
+
+  /**
+   * 
+   * @param x
+   * @param y
+   * @param z
+   * @param image
+   * @param g3d
+   * @param jmolRenderer
+   * @param antialias
+   *        UNUSED
+   * @param argbCurrent
+   * @param imageWidth
+   * @param imageHeight
+   */
+  static void plotImage(int x, int y, int z, Object image, Graphics3D g3d,
+                        JmolRendererInterface jmolRenderer, boolean antialias,
+                        int argbCurrent, int imageWidth, int imageHeight) {
+  }
+
+}

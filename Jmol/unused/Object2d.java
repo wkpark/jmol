76e18eb15a1a141b3cf6331c535586420526d6cf
Bob Hanson <hansonr@stolaf.edu>
2016-05-08 18:17:41 +0000

    Former-commit-id: 08f270977ee55be1ba7bcf32b16d0f9169047a6d

---
+++ Jmol/unused/Object2d.java
@@ -0,0 +1,11 @@
+package org.jmol.modelset;
+
+import org.jmol.java.BS;
+import org.jmol.util.C;
+import javajs.util.P3;
+import org.jmol.viewer.JC;
+
+public abstract class Object2d {
+
+ // methods incorporated into org.jmol.modelset.Text 
+}
---
+++ Jmol/unused/Object2dShape.java
@@ -0,0 +1,24 @@
+package org.jmol.shape;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+import org.jmol.java.BS;
+import org.jmol.modelset.Object2d;
+import org.jmol.modelset.Text;
+import org.jmol.util.C;
+import org.jmol.util.Logger;
+
+import javajs.api.GenericPlatform;
+import javajs.awt.Font;
+import javajs.util.Lst;
+import javajs.util.P3;
+import javajs.util.PT;
+
+public abstract class Object2dShape extends Shape {
+
+  // methods incorporated into TextShape
+}

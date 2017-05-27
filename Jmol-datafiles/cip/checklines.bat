6465e2b752d7ef6bc94f1ed15247ad8bc0048f8e
Bob Hanson <hansonr@stolaf.edu>
2017-05-27 16:05:13 +0000

    CIP validation file update
    Former-commit-id: 71146690598b753e8f604c912228c726c8d2df64

--- Jmol-datafiles/cip/checklines.bat
+++ Jmol-datafiles/cip/checklines.bat
@@ -1,6 +1,6 @@
-type ..\..\workspace\jmol\src\org\jmol\symmetry\CIPChirality.java | find /V "}"|find /V "//" |find /V "import" | find /V "*" |find ";"|find /V "Logger"  > t
+type ..\..\workspace\jmol\src\org\jmol\symmetry\CIPChirality.java | find /V "}"|find /V "//" |find /V "import" | find /V "*" |find ";"|find /V "Logger"|find /V "System.out"  > t
 
 
-type ..\..\workspace\jmol\src\org\jmol\symmetry\CIPChirality.java | find /V "}"|find /V "//" |find /V "import" | find /V "*" |find "if"|find /V "Logger" >> t
+type ..\..\workspace\jmol\src\org\jmol\symmetry\CIPChirality.java | find /V "}"|find /V "//" |find /V "import" | find /V "*" |find "if"|find /V "Logger"|find /V "System.out"  >> t
 
 type t |find " " /C
